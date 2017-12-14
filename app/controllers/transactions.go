6a70cc17c2fa4f3b4e57c65ef8609313505bb0ce
Daniel E. Renfer <duck@kronkltd.net>
2017-12-13 19:53:06 -0500

    show api endpoints

--- app/controllers/transactions.go
+++ app/controllers/transactions.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/duck1123/dinsro/app/models"
 	"github.com/duck1123/dinsro/app/services"
 	"github.com/revel/modules/orm/gorp/app/controllers"
 	"github.com/revel/revel"
@@ -28,4 +29,14 @@ func (c Transactions) IndexApi() revel.Result {
 		panic(err)
 	}
 	return c.RenderJSON(transactions)
+}
+
+func (c Transactions) Show(id uint32) revel.Result {
+	transaction := models.Transaction{Id: 1}
+	return c.Render(transaction)
+}
+
+func (c Transactions) ShowApi(id uint32) revel.Result {
+	transaction := models.Transaction{Id: 1}
+	return c.RenderJSON(transaction)
 }
