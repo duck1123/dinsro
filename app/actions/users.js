4a1c0dc53861569e293cf664c781dd5c937a6775
Daniel E. Renfer <duck@kronkltd.net>
2017-12-23 23:35:55 -0500

    user actions

--- app/actions/users.js
+++ app/actions/users.js
@@ -19,26 +19,6 @@ export const usersFetchDataSuccess = (users) => {
   };
 };
 
-export const userFetchData = (id) => {
-  return (dispatch) => {
-    dispatch(usersIsLoading(true));
-
-    fetch(`/api/v1/users/${id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-
-        dispatch(usersIsLoading(false));
-
-        return response;
-      })
-      .then(response => response.json())
-      .then(users => dispatch(usersFetchDataSuccess(users)))
-      .catch(() => dispatch(usersHasErrored(true)));
-  };
-};
-
 export const usersFetchData = (token) => {
   return (dispatch) => {
     dispatch(usersAreLoading(true));
