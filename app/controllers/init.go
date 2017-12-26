8be649a9c7c25f20ca48437b495cb571a88ba3f8
Daniel E. Renfer <duck@kronkltd.net>
2017-12-26 12:02:41 -0500

    jwt-go

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,47 +1,45 @@
 package controllers
 
 import (
-	"github.com/duck1123/dinsro/app/models"
-	"github.com/duck1123/dinsro/app/services"
-	"github.com/revel/modules/orm/gorp/app"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/revel/revel"
+	"log"
 	"net/http"
 	"strings"
 )
 
-func GetUserService() services.UserService {
-	return services.UserService{Db: gorp.Db}
+func AddLog(c *revel.Controller) revel.Result {
+	log.Println("InterceptFunc Test.")
+	return nil
 }
 
-func GetTransactionsService() services.TransactionService {
-	return services.TransactionService{Db: gorp.Db}
-}
-
+// Authenticate is and method will be called before any authenticate needed action.
+// In order to valid the user.
 func Authenticate(c *revel.Controller) revel.Result {
 	tokenString, err := getTokenString(c)
 	if err != nil {
+		log.Println("get token string failed")
 		c.Response.Status = http.StatusBadRequest
-		return c.RenderJSON(models.AuthenticationError{Message: "get token string failed"})
+		return c.RenderJSON("get token string failed")
 	}
 
-	claims, err := decodeToken(tokenString)
+	var claims jwt.MapClaims
+	claims, err = decodeToken(tokenString)
 	if err != nil {
 		c.Response.Status = http.StatusUnauthorized
-		return c.RenderJSON(models.AuthenticationError{Message: "auth failed"})
+		return c.RenderJSON("auth failed")
 	}
-
 	email, found := claims["email"]
 	if !found {
+		log.Println(err)
 		c.Response.Status = http.StatusBadRequest
-		return c.RenderJSON(models.AuthenticationError{Message: "email not found in db"})
+		return c.RenderJSON("email not found in db")
 	}
-
-	_, err = GetUserService().GetByEmail(email.(string))
+	_, err = getUser(email.(string))
 	if err != nil {
 		c.Response.Status = http.StatusUnauthorized
-		return c.RenderJSON(models.AuthenticationError{Message: "auth failed"})
+		return c.RenderJSON("auth failed")
 	}
-
 	return nil
 }
 
@@ -55,12 +53,17 @@ func getTokenString(c *revel.Controller) (tokenString string, err error) {
 	if len(tokenSlice) != 2 {
 		return "", errInvalidTokenFormat
 	}
-
 	tokenString = tokenSlice[1]
 	return tokenString, nil
 }
 
 func init() {
-	revel.InterceptFunc(Authenticate, revel.BEFORE, &Transactions{})
-	revel.InterceptFunc(Authenticate, revel.BEFORE, &Users{})
+	revel.OnAppStart(InitDB1)
+	// test
+	revel.InterceptFunc(AddLog, revel.BEFORE, &App{})
+
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &App{})
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
 }
--- app/controllers/users.go
+++ app/controllers/users.go
@@ -1,16 +1,137 @@
 package controllers
 
 import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/duck1123/dinsro/app/models"
+	"github.com/duck1123/dinsro/app/services"
 	"github.com/revel/modules/orm/gorp/app/controllers"
 	"github.com/revel/revel"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"time"
 )
+
+var hmacSecret = []byte{97, 48, 97, 50, 97, 98, 105, 49, 99, 102, 83, 53, 57, 98, 52, 54, 97, 102, 99, 12, 12, 13, 56, 34, 23, 16, 78, 67, 54, 34, 32, 21}
 
 type Users struct {
 	gorpController.Controller
 }
 
+func (c Users) getService() services.UserService {
+	return services.UserService{Db: c.Db}
+}
+
+func (c Users) getTransactionsService() services.TransactionService {
+	return services.TransactionService{Db: c.Db}
+}
+
+// Register create a user and returns token to client.
+// params: email, password
+// result: token with user.id stores in `sub` field.
+func (c Users) Register() revel.Result {
+	// create user use, email, password
+	// return token to user
+	email := c.Params.Get("email")
+	password := c.Params.Get("password")
+
+	if email == "" || password == "" {
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJSON("params is not valid.")
+	}
+
+	// check if the email have already exists in DB
+	results, err := Dbm.Select(models.User{}, `select * from User where Email = ?`, email)
+	if err != nil {
+		log.Println(err)
+	}
+	var users []*models.User
+	for _, r := range results {
+		u := r.(*models.User)
+		users = append(users, u)
+	}
+	if users != nil {
+		c.Response.Status = http.StatusConflict
+		return c.RenderJSON("user have already exists.")
+	}
+
+	// Crete user struct
+	bcryptPassword, _ := bcrypt.GenerateFromPassword(
+		[]byte(password), bcrypt.DefaultCost)
+
+	token := encodeToken(email)
+
+	newUser := &models.User{0, "Demo User", email, password, bcryptPassword, []byte(token)}
+
+	// Validate user struct
+	newUser.Validate(c.Validation)
+	if c.Validation.HasErrors() {
+		log.Println(c.Validation.Errors[0].Message)
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJSON("bad email address.")
+	}
+
+	// Save user info to DB
+	if err := Dbm.Insert(newUser); err != nil {
+		panic(err)
+	}
+
+	msg := make(map[string]string)
+	msg["email"] = email
+	msg["result"] = "user created"
+	msg["token"] = token
+	return c.RenderJSON(msg)
+}
+
+// Login authticate via email and password, if the user is valid,
+// returns the token to client.
+func (c Users) Login() revel.Result {
+	email := c.Params.Get("email")
+	password := c.Params.Get("password")
+
+	user, err := getUser(email)
+	if err != nil {
+		log.Println(err)
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJSON("invalid email or password")
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
+	if err != nil {
+		log.Println(err)
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJSON("invalid email or password")
+	}
+
+	// get token
+	tokenString := encodeToken(email)
+
+	msg := make(map[string]string)
+	msg["result"] = "login success"
+	msg["token"] = tokenString
+	c.Response.Status = http.StatusCreated
+	return c.RenderJSON(msg)
+}
+
+func getUser(email string) (*models.User, error) {
+	log.Println("before select", email)
+	users, err := Dbm.Select(models.User{}, `select * from User where Email = ?`, email)
+	log.Println("after select 1")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	log.Println("after select 2")
+	if len(users) == 0 {
+		return nil, errors.New("user not found")
+	}
+	return users[0].(*models.User), nil
+}
+
 func (c Users) IndexApi() revel.Result {
-	users, err := GetUserService().Index()
+	users, err := c.getService().Index()
 	if err != nil {
 		panic(err)
 	}
@@ -18,7 +139,7 @@ func (c Users) IndexApi() revel.Result {
 }
 
 func (c Users) ShowApi(id uint32) revel.Result {
-	user, err := GetUserService().Get(id)
+	user, err := c.getService().Get(id)
 	if err != nil {
 		panic(err)
 	}
@@ -31,4 +152,41 @@ func (c Users) IndexTransactions(id uint32) revel.Result {
 		panic(err)
 	}
 	return c.RenderJSON(transactions)
+}
+
+func encodeToken(email string) string {
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"nbf":   time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(hmacSecret)
+
+	fmt.Println(tokenString, err)
+
+	return tokenString
+}
+
+func decodeToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return hmacSecret, nil
+	})
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		fmt.Println("email and nbf:", claims["email"], claims["nbf"])
+	} else {
+		log.Println(err)
+		return nil, err
+	}
+	return claims, nil
+	// return claims["email"].(string), claims["nbf"].(string)
 }
