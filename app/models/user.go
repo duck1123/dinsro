d5cc2a0523a4e4ad35caa533f0cbccab3ca37470
Daniel E. Renfer <duck@kronkltd.net>
2017-12-27 12:27:46 -0500

    user validation

--- app/models/user.go
+++ app/models/user.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"github.com/revel/revel"
+	"regexp"
 	"time"
 )
 
@@ -15,4 +18,37 @@ type User struct {
 
 func (User) TableName() string {
 	return "users"
+}
+
+func (user *User) String() string {
+	return fmt.Sprintf("User(%s)", user.Email)
+}
+
+var emailRegexp = regexp.MustCompile(`[a-zA-Z0-9_\-]+@[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+[a-zA-Z0-9]+$`)
+
+func (user *User) Validate(v *revel.Validation) {
+	ValidateEmail(v, user.Email).Key("user.Email")
+
+	ValidatePassword(v, user.Password).
+		Key("user.Password")
+
+	v.Check(user.Name,
+		revel.Required{},
+		revel.MaxSize{Max: 100},
+	)
+}
+
+func ValidatePassword(v *revel.Validation, password string) *revel.ValidationResult {
+	return v.Check(password,
+		revel.Required{},
+		revel.MaxSize{Max: 15},
+		revel.MinSize{Min: 5},
+	)
+}
+
+func ValidateEmail(v *revel.Validation, email string) *revel.ValidationResult {
+	return v.Check(email,
+		revel.Required{},
+		revel.Match{Regexp: emailRegexp},
+	)
 }
