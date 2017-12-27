918b3e71297f6f557ff30e63e949630e00c92115
Daniel E. Renfer <duck@kronkltd.net>
2017-12-27 11:24:53 -0500

    add material ui

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,7 +1,15 @@
+import Paper from 'material-ui/Paper';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 
 const Root = () => {
-  return <p>Root Component</p>;
+  return (
+    <MuiThemeProvider>
+      <Paper>
+        <p>Root Component</p>
+      </Paper>
+    </MuiThemeProvider>
+  );
 };
 
 export default Root;
