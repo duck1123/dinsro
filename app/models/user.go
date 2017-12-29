46c30542044032309a9a7fbc0ffec6ae6d20f78a
Daniel E. Renfer <duck@kronkltd.net>
2017-12-29 17:29:47 -0500

    add properties to user model

--- app/models/user.go
+++ app/models/user.go
@@ -1,10 +1,41 @@
 package models
 
+import (
+	"fmt"
+	"github.com/revel/revel"
+	"regexp"
+)
+
 type User struct {
-	Id   uint32 `json:"id" xml:"id" db:"id, primarykey, autoincrement"`
-	Name string `json:"name" xml:"name" db:"name"`
+	Id           uint32 `json:"id" db:"id, primarykey, autoincrement"`
+	Name         string `json:"name" db:"name"`
+	Email        string `json:"email" db:"email"`
+	PasswordHash []byte `json:"-" db:"passwordHash"`
+	Token        []byte `json:"token" db:"token"`
 }
 
 func (User) TableName() string {
 	return "users"
+}
+
+func (user *User) String() string {
+	return fmt.Sprintf("User(%s)", user.Email)
+}
+
+var emailRegexp = regexp.MustCompile(`[a-zA-Z0-9_\-]+@[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+[a-zA-Z0-9]+$`)
+
+func (user *User) Validate(v *revel.Validation) {
+	ValidateEmail(v, user.Email).Key("user.Email")
+
+	v.Check(user.Name,
+		revel.Required{},
+		revel.MaxSize{Max: 100},
+	)
+}
+
+func ValidateEmail(v *revel.Validation, email string) *revel.ValidationResult {
+	return v.Check(email,
+		revel.Required{},
+		revel.Match{Regexp: emailRegexp},
+	)
 }
