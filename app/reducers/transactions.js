74339ee531d38d7a339349c9727a3ce937f6334d
Daniel E. Renfer <duck@kronkltd.net>
2018-01-15 10:32:10 -0500

    transaction reducer switch

--- app/reducers/transactions.js
+++ app/reducers/transactions.js
@@ -1,10 +1,29 @@
-export default (state = {
+const defaultState = {
   data: [
     { id: 1 },
     { id: 2 },
     { id: 3 },
     { id: 4 },
   ],
-}) => {
-  return state;
+};
+
+export default (state = defaultState, action) => {
+  console.log('state:', state);
+  console.log('action:', action);
+  switch (action.type) {
+    case 'USER_TRANSACTIONS_HAVE_ERRORED':
+      return {
+        data: [],
+      };
+    case 'USER_TRANSACTIONS_ARE_LOADING':
+      return {
+        data: [],
+      };
+    case 'USER_TRANSACTIONS_FETCH_DATA_SUCCESS':
+      return {
+        data: action.transactions,
+      };
+    default:
+      return state;
+  }
 };
