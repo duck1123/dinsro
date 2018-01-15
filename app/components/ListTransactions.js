9a9720b1232dee13f23457d52254aa407252f27d
Daniel E. Renfer <duck@kronkltd.net>
2018-01-15 12:01:21 -0500

    list transactions reducers

--- app/components/ListTransactions.js
+++ app/components/ListTransactions.js
@@ -3,17 +3,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ListTransactions extends Component {
+  static propTypes = {
+    transactions: PropTypes.arrayOf(Object).isRequired,
+  }
+
   render() {
+    const { transactions } = this.props;
     return (
       <div>
         <h1>List Transactions</h1>
+        <ul>
+          { transactions.map(transaction => (
+            <li key={transaction.id} >
+              {transaction.id}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
+ListTransactions.propTypes = {
+  transactions: PropTypes.arrayOf(Object).isRequired,
+};
+
 const mapStateToProps = (state) => {
   return {
+    transactions: state.transactions.data,
   };
 };
 
