72eebae00067e2a97bfbce763705122016e5d226
Daniel E. Renfer <duck@kronkltd.net>
2018-01-21 12:29:09 -0500

    checkpoint

--- app/models/transaction.go
+++ app/models/transaction.go
@@ -1,16 +1,23 @@
 package models
 
-import "time"
+import (
+	"github.com/revel/revel"
+	"time"
+)
 
 type Transaction struct {
-	Id        uint32    `json:"id"      db:"id, primarykey, autoincrement"`
+	Id        uint32    `json:"id"         db:"id, primarykey, autoincrement"`
 	AccountId uint32    `json:"accountId"  db:"accountId"`
-	UserId    uint32    `json:"userId"  db:"userId"`
-	Value     uint32    `json:"value"   db:"value"`
-	Created   time.Time `json:"created" db:"created"`
-	Updated   time.Time `json:"updated" db:"updated"`
+	UserId    uint32    `json:"userId"     db:"userId"`
+	Value     uint32    `json:"value"      db:"value"`
+	Created   time.Time `json:"created"    db:"created"`
+	Updated   time.Time `json:"updated"    db:"updated"`
 }
 
 func (Transaction) TableName() string {
 	return "transactions"
+}
+
+func (transaction *Transaction) Validate(v *revel.Validation) {
+	v.Required(transaction.Value)
 }
