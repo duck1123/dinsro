c225c7b2ceafaa9c78ea13141b7b9ee5cdb18e31
Daniel E. Renfer <duck@kronkltd.net>
2018-01-27 16:57:33 -0500

    return http error if transaction is not valid

--- app/controllers/transactions.go
+++ app/controllers/transactions.go
@@ -5,6 +5,7 @@ import (
 	"github.com/duck1123/dinsro/app/models"
 	"github.com/revel/modules/orm/gorp/app/controllers"
 	"github.com/revel/revel"
+	"net/http"
 )
 
 type Transactions struct {
@@ -31,7 +32,14 @@ func (c Transactions) ShowApi(id uint32) revel.Result {
 
 func (c Transactions) CreateApi(transaction models.Transaction) revel.Result {
 	transaction.UserId = c.Args["userId"].(uint32)
+	transaction.Validate(c.Validation)
 	c.Validation.Required(transaction.AccountId).Message("Account ID is required")
+
+	if c.Validation.HasErrors() {
+		c.Response.SetStatus(http.StatusBadRequest)
+		return c.RenderText("oops")
+	}
+
 	GetTransactionsService().Create(&transaction)
 	return c.RenderJSON(transaction)
 }
