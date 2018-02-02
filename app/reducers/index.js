0aed7b82ee096816a35b1bf98ccd153d4b145b4c
Daniel E. Renfer <duck@kronkltd.net>
2018-02-01 21:14:40 -0500

    remove unused reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,24 +1,18 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
-import account from './account';
 import accountTransactions from './accounttransactions';
-import accounts from './accounts';
 import authentication from './authentication';
 import models from './models';
 import transactions from './transactions';
-import user from './user';
 import useraccounts from './useraccounts';
 import users from './users';
 
 export default combineReducers({
-  account,
   accountTransactions,
-  accounts,
   authentication,
   form,
   models,
   transactions,
-  user,
   useraccounts,
   users,
 });
