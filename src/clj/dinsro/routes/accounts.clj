06704fbb91c1d6f3f2fd50ad8ab25d62560dd579
Daniel E. Renfer <duck@kronkltd.net>
2018-06-13 22:20:05 -0400

    accounts routes

--- src/clj/dinsro/routes/accounts.clj
+++ src/clj/dinsro/routes/accounts.clj
@@ -1,5 +1,8 @@
 (ns dinsro.routes.accounts
   (:require [compojure.api.sweet :refer [context GET POST DELETE PATCH]]
+            [crypto.password.bcrypt :as bcrypt]
+            [dinsro.actions.account.read-account :refer [read-account-response]]
+            [dinsro.actions.account.list-account :refer [list-account-response]]
             [dinsro.db.core :as db]
             [dinsro.middleware.authenticated :refer [authenticated-mw]]
             [dinsro.models :as m]
@@ -13,6 +16,12 @@
 
     (GET "/" []
       :return [m/Account]
+      ;; :header-params [authorization :- ::auth-header]
       :summary "Index accounts"
-      (let [accounts (db/list-accounts)]
-        (ok accounts)))))
+      (list-account-response))
+
+    (GET "/:accountId" []
+      :return m/Account
+      :summary "Read Account"
+      :path-params [accountId :- s/Int]
+      (read-account-response accountId))))
