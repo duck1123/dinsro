7a81aa894c7730114cefde60dbf5317e3df79820
Daniel E. Renfer <duck@kronkltd.net>
2018-07-04 15:00:55 -0400

    extract site info

--- src/clj/dinsro/routes/services.clj
+++ src/clj/dinsro/routes/services.clj
@@ -3,9 +3,12 @@
             [compojure.api.sweet :refer :all]
             [schema.core :as s]))
 
+(def site-info
+  {:version "1.0.0"
+   :title "Dinsro"
+   :description "Sample Services"})
+
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
-             :data {:info {:version "1.0.0"
-                           :title "Dinsro"
-                           :description "Sample Services"}}}})
+             :data {:info site-info}}})
