d47dab01dddc24cb27e363ef9cb4f40d78cb3b61
Daniel E. Renfer <duck@kronkltd.net>
2018-10-09 22:01:21 -0400

    Check password when authenticating

--- src/clj/dinsro/actions/authentication.clj
+++ src/clj/dinsro/actions/authentication.clj
@@ -1,14 +1,30 @@
 (ns dinsro.actions.authentication
-  (:require [dinsro.actions.user.create-user :refer [create-user-response]]
+  (:require [clojure.spec.alpha :as s]
+            [crypto.password.bcrypt :as bcrypt]
+            [dinsro.actions.user.create-user :refer [create-user-response]]
+            [dinsro.db.core :as db]
+            [dinsro.specs :as specs]
             [ring.util.http-response :refer :all]))
 
 (defn authenticate
   [authentication-data]
   (let [{:keys [email password]} authentication-data]
+    (let [user (db/read-user {:id 1})
+          {:keys [password-hash]} user
+          {:keys [password]} authentication-data
+          hassed-password (bcrypt/check password password-hash)]
+      (println hassed-password))
     (ok)))
+
+(s/fdef authenticate
+  :args (s/cat :authentication-data :dinsro.specs/authentication-data))
 
 (defn register
   "Register a user"
   [data]
+  {:pre [(s/valid? :dinsro.specs/register-request data)]}
   (create-user-response data)
   (ok))
+
+(s/fdef register
+  :args (s/cat :data :dinsro.specs/register-request))
