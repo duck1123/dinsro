009f7af4a19ff660047519490ca894b46e3879a2
Daniel E. Renfer <duck@kronkltd.net>
2019-08-10 14:15:49 -0400

    remove prepare user

--- src/clj/dinsro/routes/services.clj
+++ src/clj/dinsro/routes/services.clj
@@ -1,25 +1,9 @@
 (ns dinsro.routes.services
   (:require [compojure.api.sweet :refer :all]
             [dinsro.routes.accounts :refer [account-routes]]
-            ;; [dinsro.routes.authentication :refer [authentication-routes]]
             [dinsro.routes.users :refer [users-routes]]))
-
-(defn prepare-user
-  [registration-data]
-  (let [{:keys [password]} registration-data]
-    (merge {:password_hash (bcrypt/encrypt password)}
-           registration-data)))
 
 (def site-info
   {:version "1.0.0"
    :title "Dinsro"
    :description "Sample Services"})
-
-;; (def service-routes
-;;   (api
-;;    {:swagger {:ui "/swagger-ui"
-;;               :spec "/swagger.json"
-;;               :data {:info site-info}}}
-;;    account-routes
-;;    authentication-routes
-;;    users-routes))
