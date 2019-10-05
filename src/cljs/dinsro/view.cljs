f65cab07b0c6dfaf316f38a7142c045850defd69
Daniel E. Renfer <duck@kronkltd.net>
2019-10-05 12:35:31 -0400

    Add auth-toggle button for debugging

--- src/cljs/dinsro/view.cljs
+++ src/cljs/dinsro/view.cljs
@@ -3,11 +3,20 @@
             [kee-frame.core :as kf]
             [markdown.core :refer [md->html]]
             [re-frame.core :as rf]
-            [reagent.core :as r]))
+            [reagent.core :as r]
+            [taoensso.timbre :as timbre]))
+
+;; Subscriptions
 
 (rf/reg-sub
  :navbar-expanded
  (fn [db _] (get db :navbar-expanded)))
+
+(rf/reg-sub
+ :authenticated
+ (fn [db _] (get db :authenticated)))
+
+;; Events
 
 (rf/reg-event-db
  :toggle-navbar
@@ -15,19 +24,37 @@
    ;; TODO: I think this is a stream
    (assoc db :navbar-expanded (not (get db :navbar-expanded)))))
 
+(rf/reg-event-db
+ :toggle-auth
+ (fn [db [_ _]]
+   (timbre/warn "This shouldn't be available")
+   (update-in db [:authenticated] not)))
+
+;; Components
+
 (defn nav-link [title page]
   [:a.navbar-item
    {:href   (kf/path-for [page])
     :class (when (= page @(rf/subscribe [:nav/page])) "is-active")}
    title])
 
+(defn auth-toggle-button
+  []
+  (let [authenticated @(rf/subscribe [:authenticated])]
+    [:a.navbar-item
+     {:role :button
+      :on-click #(rf/dispatch [:toggle-auth])}
+     (if authenticated "in" "out")]))
+
 (defn navbar []
-  (let [expanded? @(rf/subscribe [:navbar-expanded])]
+  (let [authenticated @(rf/subscribe [:authenticated])
+        expanded? @(rf/subscribe [:navbar-expanded])]
     [:nav.navbar.is-info>div.container {:role "navigation" :aria-label "main navigation"}
      [:div.navbar-brand
       [:a.navbar-item
        {:href "/" :style {:font-weight :bold}}
        "Dinsro"]
+      [auth-toggle-button]
       [:div.navbar-burger.burger
        {:role :button
         :aria-label :menu
