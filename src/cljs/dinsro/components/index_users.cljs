e9ed8c783d9db4aecd5d5201fd3c86c73b3ca327
Daniel E. Renfer <duck@kronkltd.net>
2019-10-06 10:33:32 -0400

    index users controller

--- src/cljs/dinsro/components/index_users.cljs
+++ src/cljs/dinsro/components/index_users.cljs
@@ -1,13 +1,32 @@
-(ns dinsro.components.index-users)
+(ns dinsro.components.index-users
+  (:require [day8.re-frame.tracing :refer-macros [fn-traced]]
+            [kee-frame.core :as kf]
+            [re-frame.core :as rf]
+            [taoensso.timbre :as timbre]))
+
+(rf/reg-sub ::items (fn [db _] (get db ::items [])))
+
+(rf/reg-event-db
+ ::init-component
+ (fn-traced
+  [db _]
+  (assoc db ::users [])))
 
 (defn index-users
   []
-  (let [users []]
+  (let [users @(rf/subscribe [::items])]
     [:div
-     [:p (count users)]]))
+     [:p (count users)]
+     [:p (str users)]]))
+
+(kf/reg-controller
+ ::page
+ {:id ::page
+  :params (constantly nil)
+  :start [::init-component]})
 
 (defn page
   []
-  [:div
+  [:section.section>div.container>div.content
    [:h1 "Users Page"]
    [index-users]])
