06bfc49367ef78ecb9f5d01bcd22c4888adf6fc9
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-10-14 23:07:55 -0400

    extract authentication events

--- src/cljs/dinsro/components/login.cljs
+++ src/cljs/dinsro/components/login.cljs
@@ -3,14 +3,17 @@
             [clojure.string :as string]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
             [dinsro.components :as c]
+            [dinsro.events.authentication :as e.authentication]
             [kee-frame.core :as kf]
             [re-frame.core :as rf]
             [reagent.core :as r]
             [taoensso.timbre :as timbre]))
 
 (rf/reg-sub ::email    ::email)
+(rf/reg-event-db :change-email (fn [db [_ email]] (assoc db ::email email)))
+
 (rf/reg-sub ::password ::password)
-(rf/reg-sub :state     :state)
+(rf/reg-event-db :change-password (fn [db [_ password]] (assoc db ::password password)))
 
 (rf/reg-sub
  ::login-data
@@ -30,50 +33,14 @@
  (fn [email _] (string/blank? email)))
 
 (rf/reg-event-db
- :change-email
- (fn-traced [db [_ email]]
-   (assoc db ::email email)))
-
-(rf/reg-event-db
- :change-password
- (fn-traced [db [_ password]]
-   (assoc db ::password password)))
-
-(rf/reg-event-db
  :login-no-email
  (fn-traced [db [_ _]]
    (assoc db :no-email true)))
 
 (rf/reg-event-fx
- :login-failed
- (fn [{:keys [db]} event]
-   {:db (-> db
-            (assoc :login-failed true)
-            (assoc :loading false))}))
-
-(rf/reg-event-db
- :login-succeeded
- (fn [db [_ {:keys [identity]}]]
-   (assoc db :authenticated identity)))
-
-(rf/reg-event-fx
- ::do-authenticate
- (fn [_ [_ data]]
-   {:http-xhrio
-    {:method :post
-     :uri "/api/v1/authenticate"
-     :params data
-     :timeout 8000
-     :format          (ajax/json-request-format)
-     :response-format (ajax/json-response-format {:keywords? true})
-     :on-success [:login-succeeded]
-     :on-failure [:login-failed]}}))
-
-(rf/reg-event-fx
  :login-click
- (fn-traced [{:keys [db]} _]
-   {:db (assoc db :loading true)
-    :dispatch [::do-authenticate @(rf/subscribe [::login-data])]}))
+ (fn [_ _]
+   {:dispatch [::e.authentication/do-authenticate @(rf/subscribe [::login-data])]}))
 
 (defn page []
   [:section.section>div.container>div.content
---
+++ src/cljs/dinsro/events/authentication.cljs
@@ -0,0 +1,35 @@
+(ns dinsro.events.authentication
+  (:require [ajax.core :as ajax]
+            [clojure.string :as string]
+            [day8.re-frame.tracing :refer-macros [fn-traced]]
+            [dinsro.components :as c]
+            [kee-frame.core :as kf]
+            [re-frame.core :as rf]
+            [reagent.core :as r]
+            [taoensso.timbre :as timbre]))
+
+(kf/reg-event-db
+ ::do-authenticate-success
+ (fn [db [_ {:keys [identity]}]]
+   (assoc db :authenticated identity)))
+
+(kf/reg-event-fx
+ ::do-authenticate-failure
+ (fn [{:keys [db]} event]
+   {:db (-> db
+            (assoc ::login-failed true)
+            (assoc ::loading false))}))
+
+(kf/reg-event-fx
+ ::do-authenticate
+ (fn [{:keys [db]} [_ data]]
+   {:db (assoc db ::loading true)
+    :http-xhrio
+    {:method          :post
+     :uri             "/api/v1/authenticate"
+     :params          data
+     :timeout         8000
+     :format          (ajax/json-request-format)
+     :response-format (ajax/json-response-format {:keywords? true})
+     :on-success      [::do-authenticate-success]
+     :on-failure      [::do-authenticate-failure]}}))
