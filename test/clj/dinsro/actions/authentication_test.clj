2d74085677a524a5d47f5aec5172b238da1a743c
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-10-20 18:45:19 -0400

    fix test

--- test/clj/dinsro/actions/authentication_test.clj
+++ test/clj/dinsro/actions/authentication_test.clj
@@ -6,9 +6,11 @@
             [dinsro.config :as config]
             [dinsro.db.core :as db]
             [dinsro.handler :as handler]
+            [dinsro.specs :as ds]
             [dinsro.model.user :as model.user]
             [luminus-migrations.core :as migrations]
             [mount.core :as mount]
+            [ring.mock.request :as mock]
             [ring.util.http-status :as status]
             [taoensso.timbre :as timbre]))
 
