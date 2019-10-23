c0bd8a1919400f21e83a55b26bc4335b2af926dd
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-10-22 22:14:36 -0400

    mappings routing

--- src/cljs/dinsro/routing.cljs
+++ src/cljs/dinsro/routing.cljs
@@ -1,5 +1,31 @@
 (ns dinsro.routing
-  (:require [re-frame.core :as rf]))
+  (:require [dinsro.components.about :as about]
+            [dinsro.components.login :as login]
+            [dinsro.components.register :as register]
+            [dinsro.components.settings :as settings]
+            [dinsro.views.home :as home]
+            [dinsro.views.index-accounts :as index-accounts]
+            [dinsro.views.index-currencies :as index-currencies]
+            [dinsro.views.index-rates :as index-rates]
+            [dinsro.views.index-users :as index-users]
+            [dinsro.views.show-account :as show-account]
+            [dinsro.views.show-currency :as show-currency]
+            [dinsro.views.show-user :as show-user]
+            [re-frame.core :as rf]))
+
+(def mappings
+  {:about-page            about/page
+   :home-page             home/page
+   :index-accounts-page   index-accounts/page
+   :index-currencies-page index-currencies/page
+   :index-rates-page      index-rates/page
+   :index-users-page      index-users/page
+   :login-page            login/page
+   :register-page         register/page
+   :settings-page         settings/page
+   :show-account-page     show-account/page
+   :show-currency-page    show-currency/page
+   :show-user-page        show-user/page})
 
 (def routes
   [["/"               :home-page]
--- src/cljs/dinsro/view.cljs
+++ src/cljs/dinsro/view.cljs
@@ -1,34 +1,15 @@
 (ns dinsro.view
   (:require [dinsro.components.navbar :refer [navbar]]
-            [dinsro.components.settings :as settings]
-            [dinsro.views.about :as about]
-            [dinsro.views.home :as home]
-            [dinsro.views.index-accounts :as index-accounts]
-            [dinsro.views.index-currencies :as index-currencies]
-            [dinsro.views.index-rates :as index-rates]
-            [dinsro.views.index-users :as index-users]
-            [dinsro.views.login :as login]
-            [dinsro.views.register :as register]
-            [dinsro.views.show-account :as show-account]
-            [dinsro.views.show-currency :as show-currency]
-            [dinsro.views.show-user :as show-user]
+            [dinsro.routing :as r]
             [kee-frame.core :as kf]
             [taoensso.timbre :as timbre]))
 
 (defn root-component []
   [:div
    [navbar]
-   [kf/switch-route (fn [route] (get-in route [:data :name]))
-    :about-page            about/page
-    :home-page             home/page
-    :index-accounts-page   index-accounts/page
-    :index-currencies-page index-currencies/page
-    :index-rates-page      index-rates/page
-    :index-users-page      index-users/page
-    :login-page            login/page
-    :register-page         register/page
-    :settings-page         settings/page
-    :show-account-page     show-account/page
-    :show-currency-page    show-currency/page
-    :show-user-page        show-user/page
-    nil                    [:div "Not Found"]]])
+   (->> (-> (->> r/mappings
+                 (map identity)
+                 (into [])
+                 (reduce concat []))
+            (concat [nil [:div "Not Found"]]))
+        (into [kf/switch-route (fn [route] (get-in route [:data :name]))]))])
