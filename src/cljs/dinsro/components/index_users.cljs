417cce8ead014a430779db6805b25a326f6a9851
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-10-26 21:53:07 -0400

    index users

--- src/cljs/dinsro/components/index_users.cljs
+++ src/cljs/dinsro/components/index_users.cljs
@@ -13,21 +13,24 @@
 
 (rf/reg-sub ::error-message (fn [db _] (get db ::error-message "")))
 
+(defn user-line
+  [{:keys [id name email] :as user}]
+  [:div.column
+   {:style {:border "1px black solid"
+            :margin-bottom "15px"}}
+   [:p "Id: " id]
+   [:p "Name: " [:a {:href (kf/path-for [::v.show-user/page user])} name]]
+   [:p "Email " email]
+   [:a.button {:on-click #(rf/dispatch [::do-delete-user user])} "Delete"]])
+
 (defn index-users
   [users]
-  (if (seq users)
+  (if-not (seq users)
+    [:div [:p "No Users"]]
     (into
      [:div.section]
-     (for [{:keys [id name email] :as user} users]
-       ^{:key (:id user)}
-       [:div.column
-        {:style {:border "1px black solid"
-                 :margin-bottom "15px"}}
-        [:p "Id: " id]
-        [:p "Name: " [:a {:href (kf/path-for [::v.show-user/page user])} name]]
-        [:p "Email " email]
-        [:a.button {:on-click #(rf/dispatch [::do-delete-user user])} "Delete"]]))
-    [:div [:p "No Users"]]))
+     (for [{:keys [id] :as user} users]
+       ^{:key id} [user-line user]))))
 
 (s/fdef index-users
   :args (s/cat :data ::ds/users))
