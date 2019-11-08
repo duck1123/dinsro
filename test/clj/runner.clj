ae3550cfe0eefb1a32e42c98585403c9fc65d339
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-07 22:59:32 -0500

    runner

---
+++ test/clj/runner.clj
@@ -0,0 +1,14 @@
+(ns runner
+  (:require dinsro.actions.accounts-test
+            dinsro.actions.authentication-test
+            dinsro.actions.currencies-test
+            dinsro.actions.users-test
+            dinsro.db.core-test
+            dinsro.model.account-test
+            dinsro.model.currencies-test
+            dinsro.model.user-test
+            [orchestra.spec.test :as stest]
+            )
+  )
+
+(stest/instrument)
