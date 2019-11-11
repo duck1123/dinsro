c75292d143947b97d9b7885ed1b614d5cddff944
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-10 21:26:35 -0500

    index accounts

--- src/cljs/dinsro/components/index_accounts.cljs
+++ src/cljs/dinsro/components/index_accounts.cljs
@@ -14,14 +14,18 @@
      [:div "No Accounts"]
      (into
       [:div.section]
-      (for [{:keys [id name] :as account} accounts]
-        ^{:key (:id account)}
+      (for [{:keys [db/id
+                    dinsro.model.account/currency-id
+                    dinsro.model.account/name
+                    dinsro.model.account/user-id]
+             :as account} accounts]
+        ^{:key id}
         [:div.column
          {:style {:border        "1px black solid"
                   :margin-bottom "15px"}}
          [:p id]
          [:p [:a {:href (kf/path-for [::v.show-account/page account])} name]]
-         [:p "User: " (:user_id account)]
-         [:p "Currency: " (:currency_id account)]
+         [:p "User: " user-id]
+         [:p "Currency: " currency-id]
          [:pre (str account)]
          [:a.button {:on-click #(rf/dispatch [::e.accounts/do-delete-account id])} "Delete"]])))])
