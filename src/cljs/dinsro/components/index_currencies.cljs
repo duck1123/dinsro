a59143acb5f0dfe905351390151c6baa43f76ea8
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-14 13:07:24 -0500

    index currencies

--- src/cljs/dinsro/components/index_currencies.cljs
+++ src/cljs/dinsro/components/index_currencies.cljs
@@ -1,8 +1,23 @@
 (ns dinsro.components.index-currencies
-  (:require [dinsro.views.show-currency :as show-currency]
+  (:require [dinsro.events.currencies :as e.currencies]
+            [dinsro.views.show-currency :as show-currency]
             [kee-frame.core :as kf]
             [re-frame.core :as rf]
             [taoensso.timbre :as timbre]))
+
+(defn delete-currency-button
+  [id]
+  [:a.button {:on-click #(rf/dispatch [::e.currencies/do-delete-record id])} "Delete"])
+
+(defn index-currency-line
+  [currency]
+  (let [{:keys [db/id dinsro.model.currencies/name]} currency]
+    [:div.column
+     {:style {:border        "1px black solid"
+              :margin-bottom "15px"}}
+     [:p "Id: " id]
+     [:p "Name: " [:a {:href (kf/path-for [:show-currency-page {:id id}])} name]]
+     [delete-currency-button id]]))
 
 (defn index-currencies
   [currencies]
@@ -12,10 +27,6 @@
      [:div "No Currencies"]
      (into
       [:div.section]
-      (for [{:keys [db/id dinsro.model.currencies/name] :as currency} currencies]
+      (for [{:keys [db/id] :as currency} currencies]
         ^{:key id}
-        [:div.column
-         {:style {:border        "1px black solid"
-                  :margin-bottom "15px"}}
-         [:p "Id: " id]
-         [:p "Name: " [:a {:href (kf/path-for [:show-currency-page {:id id}])} name]]])))])
+        [index-currency-line currency])))])
