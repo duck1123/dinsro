d2f5d5b7f873e73111a53cb1a2e2237b9a0af2b3
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-17 12:53:44 -0500

    checkpoint

--- src/clj/dinsro/model/user.clj
+++ src/clj/dinsro/model/user.clj
@@ -8,6 +8,16 @@
             [orchestra.core :refer [defn-spec]]
             [taoensso.timbre :as timbre]))
 
+(def attribute-list
+  '[:db/id ::s.users/name ::s.users/email ::s.users/password-hash])
+
+(def identity-attribute ::s.users/email)
+
+(def find-by-email-query
+  '[:find ?id
+    :in $ ?email
+    :where [?id ::s.users/email ?email]])
+
 (defn-spec prepare-user (s/nilable ::s.users/item)
   [params ::s.users/params]
   (when-let [password (::s.users/password params)]
@@ -16,48 +26,52 @@
         (dissoc ::s.users/password))))
 
 (defn-spec create-user! :db/id
-  [user-params ::s.users/params]
+  [params ::s.users/params]
   (let [tempid (d/tempid "user-id")]
     (let [user (prepare-user (assoc params :db/id tempid))
           response (d/transact db/*conn* {:tx-data [user]})]
      (get-in response [:tempids tempid]))))
 
-(defn-spec list-user-ids any?
+(defn-spec index-ids (s/coll-of :db/id)
   []
   (map first (d/q '[:find ?e :where [?e ::s.users/email _]] @db/*conn*)))
 
-(defn list-users
-  []
-  (->> (list-user-ids)
-       (d/pull-many @db/*conn* '[::s.users/name ::s.users/email :db/id])))
+(defn-spec read-record (s/nilable ::s.users/item)
+  [user-id :db/id]
+  (d/pull @db/*conn*  user-id))
 
-(defn-spec delete-user any?
+(defn-spec read-records (s/coll-of (s/nilable ::item))
+  [ids (s/coll-of :ds/id)]
+  (d/pull-many @db/*conn* attribute-list))
+
+(defn-spec index-records (s/coll-of ::item)
+  []
+  (read-records (index-ids)))
+
+(defn-spec delete-record any?
   [user-id :db/id]
   (d/transact db/*conn* {:tx-data [[:db/retractEntity user-id]]}))
 
 (defn-spec delete-all nil?
   []
-  (doseq [id (list-user-ids)]
-    (delete-user id)))
-
-(defn-spec read-user (s/nilable ::s.users/item)
-  [user-id :db/id]
-  (d/pull @db/*conn* '[::s.users/name ::s.users/email ::s.users/password-hash] user-id))
+  (doseq [id (index-ids)]
+    (delete-record id)))
 
 (defn-spec find-by-email (s/nilable ::s.users/item)
   [email ::s.users/email]
-  (let [query '[:find ?id
-                :in $ ?email
-                :where [?id ::s.users/email ?email]]]
-    (read-user (ffirst (d/q query @db/*conn* email)))))
+  (let [response (d/q find-by-email-query @db/*conn* email)
+        id (ffirst response)]
+    (read-user id)))
 
-(defn-spec mock-user ::s.users/item
+(defn-spec mock-record ::s.users/item
   []
   (let [params (gen/generate (s/gen ::s.users/params))
         id (create-user! params)]
     (read-user id)))
 
 (comment
+  (index-ids)
+
   (mock-user)
   (::s.users/email (first (list-users)))
 
