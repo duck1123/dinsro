52fd1a0131d86c482b43405628ab3dc097ccb73d
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-28 09:28:26 -0500

    show user

--- src/cljs/dinsro/components/show_user.cljs
+++ src/cljs/dinsro/components/show_user.cljs
@@ -1,9 +1,23 @@
-(ns dinsro.components.show-user)
+(ns dinsro.components.show-user
+  (:require [clojure.spec.alpha :as s]
+            [dinsro.components.index-accounts :refer [index-accounts]]
+            [dinsro.spec.accounts :as s.accounts]
+            [dinsro.spec.users :as s.users]
+            [orchestra.core :refer [defn-spec]]))
 
-(defn show-user
-  [{:keys [db/id dinsro.spec.users/name dinsro.spec.users/email] :as user}]
-  [:div
-   [:p (str user)]
-   [:p "Id: " id]
-   [:p "name: " name]
-   [:p "email: " email]])
+(defn-spec show-user- vector?
+  [user ::s.users/item
+   accounts (s/coll-of ::s.accounts/item)]
+  (let [{:keys [db/id dinsro.spec.users/name dinsro.spec.users/email]} user]
+    [:div
+     [:pre (str user)]
+     [:p "Id: " id]
+     [:p "name: " name]
+     [:p "email: " email]
+     [:pre (str accounts)]
+     [index-accounts accounts]]))
+
+(defn-spec show-user vector?
+  [user ::s.users/item]
+  (let [accounts [{:db/id 7}]]
+    [show-user user accounts]))
