62b5ae9e4dcb4bde7f072732fc831bf16e85c4f0
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-11-29 09:11:56 -0500

    checkpoint

--- src/cljs/dinsro/components/show_account.cljs
+++ src/cljs/dinsro/components/show_account.cljs
@@ -1,12 +1,24 @@
 (ns dinsro.components.show-account
-  (:require [dinsro.spec.accounts :as s.accounts]
-            [orchestra.core :refer [defn-spec]]))
+  (:require [dinsro.events.currencies :as e.currencies]
+            [dinsro.events.users :as e.users]
+            [dinsro.spec.accounts :as s.accounts]
+            [dinsro.spec.currencies :as s.currencies]
+            [dinsro.spec.users :as s.users]
+            [kee-frame.core :as kf]
+            [orchestra.core :refer [defn-spec]]
+            [re-frame.core :as rf]))
 
 (defn-spec show-account vector?
   [account ::s.accounts/item]
-  [:div
-   [:p "Account: " (str account)]
-   [:p "Name: " (::s.accounts/name account)]
-   [:p "User: " (str (::s.accounts/user account))]
-   [:p "Currency: " (str (::s.accounts/currency account))]
-   [:button.button.is-danger "Delete"]])
+  (let [user-id (get-in account [::s.accounts/user :db/id])
+        user @(rf/subscribe [::e.users/item user-id])
+        currency-id (get-in account [::s.accounts/currency :db/id])
+        currency @(rf/subscribe [::e.currencies/item currency-id])]
+    [:div
+     [:p "Account: " (str account)]
+     [:p "Name: " (::s.accounts/name account)]
+     [:p "User: " [:a {:href (kf/path-for [:show-user-page {:id user-id}])}
+                   (::s.users/name user)]]
+     [:p "Currency: " [:a {:href (kf/path-for [:show-currency-page {:id currency-id}])}
+                       (::s.currencies/name currency)]]
+     [:button.button.is-danger "Delete"]]))
