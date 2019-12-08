6ed6d9952c59a138c32f2c2b06e47d4d2e762983
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-08 10:52:06 -0500

    chow account

--- src/cljs/dinsro/components/show_account.cljs
+++ src/cljs/dinsro/components/show_account.cljs
@@ -4,31 +4,45 @@
             [dinsro.spec.accounts :as s.accounts]
             [dinsro.spec.currencies :as s.currencies]
             [dinsro.spec.users :as s.users]
+            [dinsro.translations :refer [tr]]
             [kee-frame.core :as kf]
             [orchestra.core :refer [defn-spec]]
             [re-frame.core :as rf]))
 
+(defn user-link
+  [user-id]
+  (if-let [user @(rf/subscribe [::e.users/item user-id])]
+    [:a {:href (kf/path-for [:show-user-page {:id user-id}])}
+     (::s.users/name user)]
+    [:span
+     [:a {:on-click #(rf/dispatch [::e.users/do-fetch-record user-id])}
+      "Not Loaded"]]))
+
+(defn currency-link
+  [currency-id]
+  (if-let [currency @(rf/subscribe [::e.currencies/item currency-id])]
+    [:a {:href (kf/path-for [:show-currency-page {:id currency-id}])}
+     (::s.currencies/name currency)]
+    [:span
+     [:a {:on-click #(rf/dispatch [::e.currencies/do-fetch-record currency-id])}
+      "Not Loaded"]]))
+
+(defn delete-account-button
+  [id]
+  [:button.button.is-danger "Delete"])
+
 (defn-spec show-account vector?
   [account ::s.accounts/item]
-  (let [user-id (get-in account [::s.accounts/user :db/id])
+  (let [id (:db/id account)
+        user-id (get-in account [::s.accounts/user :db/id])
         currency-id (get-in account [::s.accounts/currency :db/id])]
     [:div.box
-     [:pre (str account)]
+     #_[:pre (str account)]
      [:p "Name: " (::s.accounts/name account)]
      [:p
-      [:span "User: "]
-      (if-let [user @(rf/subscribe [::e.users/item user-id])]
-        [:a {:href (kf/path-for [:show-user-page {:id user-id}])}
-         (::s.users/name user)]
-        [:span
-         [:a {:on-click #(rf/dispatch [::e.users/do-fetch-record user-id])}
-          "Not Loaded"]])]
+      (tr [:user-label])
+      [user-link user-id]]
      [:p
       [:span "Currency: "]
-      (if-let [currency @(rf/subscribe [::e.currencies/item currency-id])]
-        [:a {:href (kf/path-for [:show-currency-page {:id currency-id}])}
-         (::s.currencies/name currency)]
-        [:span
-         [:a {:on-click #(rf/dispatch [::e.currencies/do-fetch-record currency-id])}
-          "Not Loaded"]])]
-     [:button.button.is-danger "Delete"]]))
+      [currency-link currency-id]]
+     [delete-account-button id]]))
