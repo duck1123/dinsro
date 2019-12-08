9a20bb31d73fb06c6bc930899c1beeb7172919bb
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-08 13:07:10 -0500

    toggle debug

--- src/cljs/dinsro/components/debug.cljs
+++ src/cljs/dinsro/components/debug.cljs
@@ -1,22 +1,26 @@
 (ns dinsro.components.debug
-  (:require
-   [clojure.spec.alpha :as s]
-   [clojure.spec.gen.alpha :as gen]
-   [dinsro.translations :refer [tr]]
-   [re-frame.core :as rf]
-   [reframe-utils.core :as rfu]
-
-   [taoensso.timbre :as timbre]
-   )
-  )
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [dinsro.translations :refer [tr]]
+            [kee-frame.core :as kf]
+            [re-frame.core :as rf]
+            [reframe-utils.core :as rfu]
+            [taoensso.timbre :as timbre]))
 
 (s/def ::debug-shown? boolean?)
 (rfu/reg-basic-sub ::debug-shown?)
+
+(defn toggle-debug
+  [{:keys [db]} _]
+  {:db (update db ::debug-shown? not)})
+
+(kf/reg-event-fx ::toggle-debug toggle-debug)
 
 (defn toggle-debug-button
   []
   (let [debug-shown? @(rf/subscribe [::debug-shown?])]
     [:a.button
+     {:on-click #(rf/dispatch [::toggle-debug])}
      (tr [:debug-shown "Debug Shown: %1"] [(str (boolean debug-shown?))])]))
 
 (defn debug-box
