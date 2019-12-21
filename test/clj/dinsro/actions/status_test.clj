0bcaf8ebe83ea5bcf2c648c37ec4e04a8deb1af2
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-21 14:03:17 -0500

    status test

--- test/clj/dinsro/actions/status_test.clj
+++ test/clj/dinsro/actions/status_test.clj
@@ -1,4 +1,21 @@
-(ns dinsro.actions.status-test)
+(ns dinsro.actions.status-test
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.test :refer :all]
+            [datahike.api :as d]
+            ;; [dinsro.actions.currencies :as a.currencies]
+            [dinsro.actions.status :as a.status]
+            [dinsro.config :as config]
+            [dinsro.db.core :as db]
+            [dinsro.mocks :as mocks]
+            ;; [dinsro.model.currencies :as m.currencies]
+            ;; [dinsro.spec.actions.currencies :as s.a.currencies]
+            ;; [dinsro.spec.currencies :as s.currencies]
+            [dinsro.specs :as ds]
+            [mount.core :as mount]
+            [ring.util.http-status :as status]
+            [taoensso.timbre :as timbre])
+  )
 
 (deftest status-handler-no-identity
   (let [request {}
