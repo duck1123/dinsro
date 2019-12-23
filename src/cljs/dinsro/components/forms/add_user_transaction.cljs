3be6c675e63f531c0c5f131230d945dcd7106306
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-23 10:31:23 -0500

    checkpoint

--- src/cljs/dinsro/components/forms/add_user_transaction.cljs
+++ src/cljs/dinsro/components/forms/add_user_transaction.cljs
@@ -1,20 +1,23 @@
 (ns dinsro.components.forms.add-user-transaction
   (:require [dinsro.components :as c]
             [dinsro.components.debug :as c.debug]
+            [dinsro.events.forms.add-user-transaction :as e.f.add-user-transaction]
+            [dinsro.spec.events.forms.add-user-transaction :as s.e.f.add-user-transaction]
             [dinsro.translations :refer [tr]]
             [re-frame.core :as rf]))
 
 (defn form
   []
-  (let [form-data @(rf/subscribe [::form-data])]
-    (when @(rf/subscribe [::shown?])
+  (let [form-data @(rf/subscribe [::e.f.add-user-transaction/form-data])]
+    (when @(rf/subscribe [::s.e.f.add-user-transaction/shown?])
       [:div
-       [c/close-button ::set-shown?]
+       [c/close-button ::s.e.f.add-user-transaction/set-shown?]
        [c.debug/debug-box form-data]
        [:p "Form"]
        [:div.field>div.control
-        [c/number-input (tr [:value]) ::value ::set-value]]
+        [c/number-input (tr [:value]) ::s.e.f.add-user-transaction/value ::s.e.f.add-user-transaction/set-value]]
        [:div.field>div.control
-        [c/currency-selector (tr [:currency]) ::currency-id ::set-currency-id]]
+        [c/currency-selector (tr [:currency])
+         ::s.e.f.add-user-transaction/currency-id ::s.e.f.add-user-transaction/set-currency-id]]
        [:div.field>div.control
         [c/primary-button (tr [:submit]) [::submit-clicked]]]])))
