4da30d5526a8ac1af3d93e2e54ac1ca219cb2b2d
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-24 11:17:07 -0500

    checkpoint

--- src/cljs/dinsro/components/forms/login.cljs
+++ src/cljs/dinsro/components/forms/login.cljs
@@ -12,6 +12,9 @@
   (let [form-data @(rf/subscribe [::s.e.f.login/form-data])]
     [:form.is-centered
      [c.debug/debug-box form-data]
-     [c/email-input (tr [:email]) ::s.e.f.login/email ::s.e.f.login/set-email]
-     [c/password-input (tr [:password]) ::s.e.f.login/password ::s.e.f.login/set-password]
-     [c/primary-button (tr [:login]) [::e.authentication/do-authenticate form-data return-to]]]))
+     [:div.field>div.control
+      [c/email-input (tr [:email]) ::s.e.f.login/email ::s.e.f.login/set-email]]
+     [:div.field>div.control
+      [c/password-input (tr [:password]) ::s.e.f.login/password ::s.e.f.login/set-password]]
+     [:div.field>div.control
+      [c/primary-button (tr [:login]) [::e.authentication/do-authenticate form-data return-to]]]]))
