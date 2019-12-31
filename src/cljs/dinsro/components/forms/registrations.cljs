3ca4d73b35908f6ae55086e2575bd71e9df1dced
Duck Nebuchadnezzar <duck@kronkltd.net>
2019-12-31 11:22:42 -0500

    registration form

--- src/cljs/dinsro/components/forms/registrations.cljs
+++ src/cljs/dinsro/components/forms/registrations.cljs
@@ -1,18 +1,24 @@
 (ns dinsro.components.forms.registration
   (:require [dinsro.components :as c]
             [dinsro.components.debug :as c.debug]
+            [dinsro.events.forms.registration :as e.f.registration]
+            [dinsro.spec.events.forms.registration :as s.e.f.registration]
             [dinsro.translations :refer [tr]]
             [re-frame.core :as rf]
             [taoensso.timbre :as timbre]))
 
 (defn form
   []
-  (let [form-data @(rf/subscribe [::form-data])]
+  (let [form-data @(rf/subscribe [::e.f.registration/form-data])]
     [:div.box
      [:form
       [c.debug/debug-box form-data]
-      [c/text-input     "Name"             ::name             ::set-name]
-      [c/email-input    "Email"            ::email            ::set-email]
-      [c/password-input "Password"         ::password         ::set-password]
-      [c/password-input "Confirm Password" ::confirm-password ::set-confirm-password]
+      [c/text-input     "Name"
+       ::s.e.f.registration/name             ::s.e.f.registration/set-name]
+      [c/email-input    "Email"
+       ::s.e.f.registration/email            ::s.e.f.registration/set-email]
+      [c/password-input "Password"
+       ::s.e.f.registration/password         ::s.e.f.registration/set-password]
+      [c/password-input "Confirm Password"
+       ::s.e.f.registration/confirm-password ::s.e.f.registration/set-confirm-password]
       [c/primary-button (tr [:submit]) [::submit-clicked form-data]]]]))
