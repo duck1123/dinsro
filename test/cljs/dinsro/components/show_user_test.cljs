e450fb9f50090b94d15495f65e86f3ab5cecd203
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-01-10 23:00:09 -0500

    Move card to show user test

---
+++ test/cljs/dinsro/components/show_user_test.cljs
@@ -0,0 +1,20 @@
+(ns dinsro.components.show-user-test
+  (:require [devcards.core :refer-macros [defcard defcard-rg]]
+            [dinsro.components.show-user :as c.show-user]
+            [dinsro.spec.users :as s.users]
+            [dinsro.translations :refer [tr]]
+            [taoensso.timbre :as timbre]))
+
+(let [user {::s.users/name "Bart"
+               ::s.users/user {:db/id 1}
+               ::s.users/currency {:db/id 1}}]
+  (defcard user user)
+
+  (defcard-rg show-user
+    [c.show-user/show-user user])
+
+  (defcard-rg show-user-with-box
+    [:div.box [c.show-user/show-user user]])
+
+  (defcard show-user2
+    (reagent.core/as-element [c.show-user/show-user user])))
