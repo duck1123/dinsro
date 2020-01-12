f2dc9a70d4b3167f386b8f50ba5c0e785fa07b76
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-01-11 21:59:38 -0500

    Fix failing test

--- test/cljs/dinsro/components/forms/add_user_transaction_test.cljs
+++ test/cljs/dinsro/components/forms/add_user_transaction_test.cljs
@@ -1,9 +1,11 @@
 (ns dinsro.components.forms.add-user-transaction-test
   (:require [devcards.core :refer-macros [defcard defcard-rg]]
             [dinsro.components.forms.add-user-transaction :as c.f.add-user-transaction]
+            [dinsro.events.forms.add-user-transaction :as e.f.add-user-transaction]
+            [dinsro.spec :as ds]
             [dinsro.translations :refer [tr]]))
 
-(defcard
+(defcard form-data
   (ds/gen-key ::e.f.add-user-transaction/form-data))
 
 (defcard-rg form
