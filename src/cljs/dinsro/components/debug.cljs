561420c4ce7529acb8cc1264cabef8cafaf0b84d
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-01-14 20:06:57 -0500

    Pretty print data in debug box

--- src/cljs/dinsro/components/debug.cljs
+++ src/cljs/dinsro/components/debug.cljs
@@ -1,5 +1,6 @@
 (ns dinsro.components.debug
   (:require
+   [cljs.pprint :as p]
    [dinsro.events.debug :as e.debug]
    [dinsro.translations :refer [tr]]
    [re-frame.core :as rf]
@@ -11,4 +12,6 @@
 
 (defn debug-box
   [data]
-  (hide [:pre (str data)]))
+  (hide
+   [:pre {:style {:max-height "200px"}}
+    (with-out-str (p/pprint data))]))
