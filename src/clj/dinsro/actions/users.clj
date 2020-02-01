a70769cf0569db75f05b626a7ceb0527577f2b21
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-02-01 06:29:48 -0500

    pluralize model users namespace

--- src/clj/dinsro/actions/users.clj
+++ src/clj/dinsro/actions/users.clj
@@ -1,14 +1,14 @@
 (ns dinsro.actions.users
   (:require
    [clojure.spec.alpha :as s]
-   [dinsro.model.users :as m.user]
+   [dinsro.model.users :as m.users]
    [dinsro.spec.actions.users :as s.a.users]
    [ring.util.http-response :as http]
    [taoensso.timbre :as timbre]))
 
 (defn create-handler
   [{:keys [params]}]
-  (or (try (if-let [user (m.user/create-record params)]
+  (or (try (if-let [user (m.users/create-record params)]
              (http/ok {:user user}))
            (catch Exception _ nil))
       (http/bad-request {:status :invalid})))
@@ -16,12 +16,12 @@
 (defn delete-handler
   [request]
   (let [user-id (Integer/parseInt (:id (:path-params request)))]
-    (m.user/delete-record user-id)
+    (m.users/delete-record user-id)
     (http/ok {:id user-id})))
 
 (defn index-handler
   [_]
-  (let [users (m.user/index-records)]
+  (let [users (m.users/index-records)]
     (http/ok {:users users})))
 
 (s/fdef index-handler
@@ -34,7 +34,7 @@
   [request ]
   (let [{{id :id} :path-params} request]
     (if-let [id (try (Integer/parseInt id) (catch NumberFormatException _ nil))]
-      (if-let [user (m.user/read-record id)]
+      (if-let [user (m.users/read-record id)]
         (http/ok {:item user})
         (http/not-found {:status :not-found}))
       (http/bad-request {:status :bad-request}))))
