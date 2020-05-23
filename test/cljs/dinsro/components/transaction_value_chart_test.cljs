c14f236b3a9f4a08e0a9455a91103a5d4e0fe4b6
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-05-23 14:28:18 -0400

    transaction value chart

---
+++ test/cljs/dinsro/components/transaction_value_chart_test.cljs
@@ -0,0 +1,43 @@
+(ns dinsro.components.transaction-value-chart-test
+  (:require
+   [clojure.spec.alpha :as s]
+   [devcards.core :refer-macros [defcard defcard-rg]]
+   [dinsro.components.transaction-value-chart :as c.transaction-value-chart]
+   [dinsro.spec :as ds]
+   [dinsro.spec.accounts :as s.accounts]
+   [dinsro.spec.currencies :as s.currencies]
+   [dinsro.spec.transactions :as s.transactions]
+   [dinsro.translations :refer [tr]]
+   [taoensso.timbre :as timbre]))
+
+(let [data [[1  1]
+            [2  2]
+            [3  4]
+            [4  8]
+            [5 16]]]
+  (defcard data data)
+
+  (let [account (ds/gen-key ::s.accounts/item)
+        account-id (:db/id account)]
+    (defcard account account)
+
+    (let [currency-id (:db/id (::s.accounts/currency account))
+          currency (and currency-id (assoc (ds/gen-key ::s.currencies/item)
+                                           :db/id currency-id))]
+      (defcard currency currency)
+
+      (let [transactions (map
+                          (fn [transaction]
+                            (assoc-in transaction [::s.transactions/account :db/id] account-id))
+                          (ds/gen-key (s/coll-of ::s.transactions/item)))]
+        (defcard transactions transactions)
+
+        (let [new-data (map
+                        (fn [transaction]
+                          [(::s.transactions/date transaction)
+                           (::s.transactions/value transaction)])
+                        transactions)]
+          (defcard new-data new-data)
+
+         (defcard-rg transaction-value-chart
+           [c.transaction-value-chart/transaction-value-chart account new-data]))))))
