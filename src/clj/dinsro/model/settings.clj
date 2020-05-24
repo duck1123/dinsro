f435bd664c52795adfe0f85d7ebbc9b7a1ca64d9
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-05-24 10:39:34 -0400

    allow registration

--- src/clj/dinsro/model/settings.clj
+++ src/clj/dinsro/model/settings.clj
@@ -1,5 +1,8 @@
-(ns dinsro.model.settings)
+(ns dinsro.model.settings
+  (:require
+   [dinsro.model.users :as m.users]))
 
 (defn get-settings
   []
-  {:allow-registration false})
+  ;; Enable Registration if there are no users
+  {:allow-registration (not (empty? (m.users/index-ids)))})
