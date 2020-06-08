14d2402e80ec45334bdfb8e4ab00ab6429442e62
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-06-07 23:52:24 -0400

    Error boundary

---
+++ src/cljs/dinsro/components/boundary.cljs
@@ -0,0 +1,21 @@
+(ns dinsro.components.boundary
+  (:require
+   [cljs.repl :as repl]
+   [reagent.core :as r]
+   [taoensso.timbre :as timbre]))
+
+(defn error-boundary
+  [& children]
+  (let [err-state (r/atom nil)]
+    (r/create-class
+     {:display-name "ErrBoundary"
+      :component-did-catch
+      (fn [component info]
+        (.error js/console info)
+        (reset! err-state [component info]))
+      :reagent-render
+      (fn [& children]
+        (if (nil? @err-state)
+          (into [:<>] children)
+          (let [[_ info] @err-state]
+            [:div.error [:pre [:code (repl/error->str info)]]])))})))
