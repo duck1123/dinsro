28d72283108a178346ee2b5a844e31a49e1bc74c
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-05 11:55:35 -0400

    Admin index categories test

--- test/cljs/dinsro/components/admin_index_categories_test.cljs
+++ test/cljs/dinsro/components/admin_index_categories_test.cljs
@@ -1,6 +1,6 @@
 (ns dinsro.components.admin-index-categories-test
   (:require
-   [cljs.test :refer [is]]
+   [cljs.test :refer-macros [is]]
    [clojure.spec.alpha :as s]
    [devcards.core :refer-macros [defcard defcard-rg deftest]]
    [dinsro.cards :as cards]
@@ -12,33 +12,49 @@
    [dinsro.events.users :as e.users]
    [dinsro.spec :as ds]
    [dinsro.spec.categories :as s.categories]
+   [dinsro.spec.users :as s.users]
    [dinsro.store :as st]
    [dinsro.store.mock :refer [mock-store]]
-   [dinsro.translations :refer [tr]]))
+   [dinsro.translations :refer [tr]]
+   [taoensso.timbre :as timbre]))
 
 (cards/header "Admin Index Categories Components" [])
 
-(let [categories (ds/gen-key (s/coll-of ::s.categories/item :count 3))
-      store (doto (mock-store)
-              e.debug/init-handlers!
-              e.categories/init-handlers!
-              e.users/init-handlers!
-              e.f.create-category/init-handlers!)]
-  (defcard categories categories)
+(def users (ds/gen-key (s/coll-of ::s.users/item :count 3)))
 
+(def categories (map
+                 (fn [category]
+                   (let [user-ids (map :db/id users)
+                         user-id (rand-nth user-ids)]
+                     (assoc-in category [::s.categories/user :db/id] user-id)))
+                 (ds/gen-key (s/coll-of ::s.categories/item :count 3))))
+(def category (first categories))
+
+(defn test-store
+  []
+  (let [store (doto (mock-store)
+                e.debug/init-handlers!
+                e.categories/init-handlers!
+                e.users/init-handlers!
+                e.f.create-category/init-handlers!)]
+    store))
+
+(comment (defcard users-card users))
+(comment (defcard categories-card categories))
+
+(let [store (test-store)]
   (st/dispatch store [::e.categories/do-fetch-index-success {:items categories}])
-
+  (st/dispatch store [::e.users/do-fetch-index-success {:users users}])
   ;; (st/dispatch store [::e.f.create-categories/set-shown? true])
 
-  (let [category (first categories)]
-    (defcard-rg c.admin-index-categories/category-line
-      (fn []
-        [error-boundary
-         [:table.table>tbody
-          [c.admin-index-categories/category-line store category]]]))
+  (defcard-rg c.admin-index-categories/category-line
+    (fn []
+      [error-boundary
+       [:table.table>tbody
+        [c.admin-index-categories/category-line store category]]]))
 
-    (deftest category-line-test
-      (is (vector? (c.admin-index-categories/category-line store category)))))
+  (deftest category-line-test
+    (is (vector? (c.admin-index-categories/category-line store category))))
 
   (defcard-rg c.admin-index-categories/index-categories
     (fn []
