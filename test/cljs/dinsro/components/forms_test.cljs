8e2626d13f2b54d0214d428f6540dd2bab1bb7f0
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-06 16:52:46 -0400

    create account test

--- test/cljs/dinsro/components/forms_test.cljs
+++ test/cljs/dinsro/components/forms_test.cljs
@@ -2,6 +2,7 @@
   (:require
    [dinsro.cards :as cards]
    [dinsro.components.forms.add-user-transaction-test]
+   [dinsro.components.forms.create-account-test]
    [dinsro.components.forms.create-transaction-test]
    [dinsro.components.forms.registration-test]
    [dinsro.components.forms.settings-test]
