4f8b405abe74d6032d7c3678d206864f55bc92a4
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-06 16:17:49 -0400

    index transaction tests

--- test/cljs/dinsro/components/index_transactions_test.cljs
+++ test/cljs/dinsro/components/index_transactions_test.cljs
@@ -1,10 +1,13 @@
 (ns dinsro.components.index-transactions-test
   (:require
+   [cljs.test :refer-macros [is]]
    [clojure.spec.alpha :as s]
-   [devcards.core :refer-macros [defcard defcard-rg]]
+   [devcards.core :refer-macros [defcard defcard-rg deftest]]
    [dinsro.cards :as cards]
    [dinsro.components.boundary :refer [error-boundary]]
    [dinsro.components.index-transactions :as c.index-transactions]
+   [dinsro.events.accounts :as e.accounts]
+   [dinsro.events.debug :as e.debug]
    [dinsro.spec :as ds]
    [dinsro.spec.transactions :as s.transactions]
    [dinsro.store.mock :refer [mock-store]]
@@ -12,19 +15,31 @@
 
 (cards/header "Index Transaction Components" [])
 
-(let [store (mock-store)]
-  (let [item (ds/gen-key ::s.transactions/item)]
-    (defcard item item)
+(let [items (ds/gen-key (s/coll-of ::s.transactions/item))
+      transaction-store (fn []
+                          (doto (mock-store)
+                            e.debug/init-handlers!
+                            e.accounts/init-handlers!))]
+
+  (let [item (first items)
+        store (transaction-store)]
+    (comment (defcard item item))
 
     (defcard-rg c.index-transactions/row-line
       (fn []
         [error-boundary
-         (c.index-transactions/row-line store item)])))
+         [c.index-transactions/row-line store item]]))
 
-  (let [items (ds/gen-key (s/coll-of ::s.transactions/item))]
-    (defcard items items)
+    (deftest row-line-test
+      (is (vector? (c.index-transactions/row-line store item)))))
+
+  (let [store (transaction-store)]
+    (comment (defcard items items))
 
     (defcard-rg c.index-transactions/index-transactions
       (fn []
         [error-boundary
-         (c.index-transactions/index-transactions store items)]))))
+         [c.index-transactions/index-transactions store items]]))
+
+    (deftest index-transactions-test
+      (is (vector? (c.index-transactions/index-transactions store items))))))
