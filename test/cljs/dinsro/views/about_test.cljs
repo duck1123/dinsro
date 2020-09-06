1ec0c4fffd6c2d49dde076066b8ddbb44968d9ea
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-06 16:59:28 -0400

    about test

--- test/cljs/dinsro/views/about_test.cljs
+++ test/cljs/dinsro/views/about_test.cljs
@@ -1,15 +1,26 @@
 (ns dinsro.views.about-test
   (:require
    [cljs.test :refer-macros [is]]
-   [devcards.core :refer-macros [deftest]]
+   [devcards.core :refer-macros [defcard-rg deftest]]
    [dinsro.cards :as cards]
+   [dinsro.components.boundary :refer [error-boundary]]
    [dinsro.store.mock :refer [mock-store]]
-   [dinsro.views.about :as about]
+   [dinsro.views.about :as v.about]
    [pjstadig.humane-test-output]))
 
 (cards/header "About View" [])
 
-(deftest about-page-test
-  (let [store (mock-store)
-        match nil]
-    (is (vector? (about/page store match)))))
+(defn test-store
+  []
+  (let [store (mock-store)]
+    store))
+
+(let [store (test-store)
+      match nil]
+
+  (defcard-rg page-card
+    [error-boundary
+     [v.about/page store match]])
+
+  (deftest page-test
+    (is (vector? (v.about/page store match)))))
