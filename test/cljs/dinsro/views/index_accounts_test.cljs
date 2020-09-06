41d67c2d4389e1d9911185b90480589727cb3f12
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-06 18:59:19 -0400

    index accounts test

--- test/cljs/dinsro/views/index_accounts_test.cljs
+++ test/cljs/dinsro/views/index_accounts_test.cljs
@@ -9,47 +9,72 @@
    [dinsro.events.authentication :as e.authentication]
    [dinsro.events.debug :as e.debug]
    [dinsro.events.currencies :as e.currencies]
+   [dinsro.events.forms.add-user-account :as e.f.add-user-account]
+   [dinsro.events.forms.create-account :as e.f.create-account]
    [dinsro.events.users :as e.users]
    [dinsro.spec :as ds]
    [dinsro.spec.accounts :as s.accounts]
+   [dinsro.spec.currencies :as s.currencies]
    [dinsro.store :as st]
    [dinsro.store.mock :refer [mock-store]]
+   [dinsro.test-utils :refer-macros [assert-spec]]
    [dinsro.views.index-accounts :as v.index-accounts]
    [reitit.core :as rc]
    [taoensso.timbre :as timbre]))
 
 (cards/header "Index Accounts View" [])
 
-(let [items (ds/gen-key (s/coll-of ::s.accounts/item :count 2))
+(defn test-app
+  []
+  (let [store (doto (mock-store)
+                e.accounts/init-handlers!
+                e.authentication/init-handlers!
+                e.currencies/init-handlers!
+                e.debug/init-handlers!
+                e.f.add-user-account/init-handlers!
+                e.f.create-account/init-handlers!
+                e.users/init-handlers!)]
+    store))
+
+(let [currencies (ds/gen-key (s/coll-of ::s.currencies/item :count 3))
+      items (ds/gen-key (s/coll-of ::s.accounts/item :count 2))
       template nil
       data nil
       result nil
       path "/accounts"
       path-params {}
-      store (doto (mock-store)
-              e.debug/init-handlers!
-              e.accounts/init-handlers!
-              e.authentication/init-handlers!
-              e.currencies/init-handlers!
-              e.users/init-handlers!
-              e.authentication/init-handlers!)
       match (rc/->Match template data result path-params path)]
 
-  (defcard items items)
+  (comment (defcard items items))
+  (comment (defcard currencies currencies))
 
-  (defcard-rg page-card
-    (fn []
-      (st/dispatch store [::e.authentication/set-auth-id 1])
-      (st/dispatch store [::e.accounts/do-fetch-index-success {:items items}])
-      [error-boundary
-       [v.index-accounts/page store match]]))
+  (let [store (test-app)
+        form-data (st/subscribe store [::e.f.add-user-account/form-data])]
+    (st/dispatch store [::e.authentication/set-auth-id 1])
+    (st/dispatch store [::e.f.add-user-account/set-shown? true])
+    (st/dispatch store [::e.accounts/do-fetch-index-success {:items items}])
+    (st/dispatch store [::e.currencies/do-fetch-index-success {:items currencies}])
 
-  (defcard-rg page-card-unauthenticated
-    (fn []
-      (st/dispatch store [::e.authentication/set-auth-id nil])
-      (st/dispatch store [::e.accounts/do-fetch-index-success {:items items}])
-      [error-boundary
-       [v.index-accounts/page store match]]))
+    (defcard-rg page-card
+      (fn []
+        [error-boundary
+         [v.index-accounts/page store match]]))
 
-  (deftest page-test
-    (is (vector? (v.index-accounts/page store match)))))
+    (defcard-rg form-data (fn [] (pr-str @form-data)))
+    (let [form-data-sub @form-data]
+      (assert-spec ::e.f.add-user-account/form-data form-data-sub))
+
+    (deftest page-test
+      (is (vector? (v.index-accounts/page store match)))))
+
+  (let [store (test-app)]
+    (st/dispatch store [::e.authentication/set-auth-id nil])
+    (st/dispatch store [::e.accounts/do-fetch-index-success {:items items}])
+
+    (defcard-rg page-unauthenticated
+      (fn []
+        [error-boundary
+         [v.index-accounts/page store match]]))
+
+    (deftest page-unauthenticated-test
+      (is (vector? (v.index-accounts/page store match))))))
