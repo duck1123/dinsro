1964ff21b7e45e47620236a8c890a50e4e41917c
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-06 21:34:42 -0400

    rate sources test

---
+++ test/cljs/dinsro/views/rate_sources_test.cljs
@@ -0,0 +1,10 @@
+(ns dinsro.views.rate-sources-test
+  (:require
+   [devcards.core :refer-macros [defcard defcard-rg]]
+   [dinsro.cards :as cards]))
+
+(cards/header "Rate Source View" [])
+
+(defcard foo
+  "foo"
+  )
