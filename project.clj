4fc09ad9fb3fd82bc39a25c6b2cec700814dc321
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-10 19:28:38 -0400

    add test cljc to path

--- project.clj
+++ project.clj
@@ -90,7 +90,7 @@
   :min-lein-version "2.0.0"
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
-  :test-paths ["test/clj"]
+  :test-paths ["test/clj" "test/cljc"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot dinsro.core
