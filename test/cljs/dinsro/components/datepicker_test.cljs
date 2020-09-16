c5fd4d4efc7d38bf6d08f8ef9e7dd95f20882b8f
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-15 22:19:16 -0400

    datepicker test

--- test/cljs/dinsro/components/datepicker_test.cljs
+++ test/cljs/dinsro/components/datepicker_test.cljs
@@ -4,17 +4,27 @@
    [devcards.core :refer-macros [defcard-rg deftest]]
    [dinsro.cards :as cards :include-macros true]
    [dinsro.components.boundary :refer [error-boundary]]
-   [dinsro.components.datepicker :as c.datepicker]))
+   [dinsro.components.datepicker :as c.datepicker]
+   [dinsro.store :as st]
+   [dinsro.store.mock :refer [mock-store]]
+   [taoensso.timbre :as timbre]))
 
 (cards/header
  'dinsro.components.datepicker-test
- "Datepicker Components" [])
+ "Datepicker Components" [#{:components}])
 
-(let [props {}]
+(let [value-key ::foo
+      setter-key ::set-foo
+      store (doto (mock-store)
+              (st/reg-basic-sub value-key)
+              (st/reg-set-event value-key))
+      props {:on-select #(st/dispatch store [setter-key %])}]
   (defcard-rg datepicker
     (fn []
       [error-boundary
-       [c.datepicker/datepicker props]]))
+       [:<>
+        [:pre (pr-str @(st/subscribe store [value-key]))]
+        [c.datepicker/datepicker props]]]))
 
   (deftest datepicker-text
     (is (vector? (c.datepicker/datepicker props)))))
