851cfa4203412e606d4c3f70db47338f8e93be3d
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-12-19 18:57:18 -0500

    add pushy dependency

--- src/fulcro/dinsro/routing.cljs
+++ src/fulcro/dinsro/routing.cljs
@@ -1,23 +1,37 @@
 (ns dinsro.routing
   (:require
+   [clojure.string :as string]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
    [com.fulcrologic.fulcro.mutations :as fm :refer [defmutation]]
    [dinsro.app :as da]
-   [dinsro.router :as router]
+   [pushy.core :as pushy]
    [taoensso.timbre :as timbre]))
 
 (defsc CurrentUser
   [_this _props]
   {:query [:identity]})
 
+(defonce history
+  (pushy/pushy
+   (fn [p]
+     (let [route-segments (vec (rest (string/split p "/")))]
+       (dr/change-route da/app route-segments)))
+   identity))
+
+(defn route-to!
+  [route-string]
+  (pushy/set-token! history route-string))
+
 (defmutation finish-login [_]
   (action
-   [{:keys [_app _state]}]
-   (timbre/info "finish login")))
+   [{:keys [_app state]}]
+   (let [logged-in? (get-in @state [:session/current-user :user/valid])]
+     (when-not logged-in?
+       (route-to! "/login"))
+     (swap! state #(assoc % :root/ready? true)))))
 
 (defn start!
   []
   (dr/initialize! da/app)
-  ;; TODO: parse from url
-  (dr/change-route-relative! da/app router/RootRouter [""]))
+  (pushy/start! history))
