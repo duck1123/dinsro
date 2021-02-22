e2c473d18941cd9b0ce9f10b8eb8cba18435b51a
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-02-22 07:37:21 -0500

    user resolver

--- src/main/dinsro/resolvers/users.clj
+++ src/main/dinsro/resolvers/users.clj
@@ -2,7 +2,7 @@
   (:require
    [com.wsscode.pathom.connect :as pc :refer [defresolver]]
    [dinsro.model.users :as m.users]
-   [dinsro.sample :as sample]
+   [dinsro.queries.users :as q.users]
    [taoensso.timbre :as timbre]))
 
 (defresolver user-resolver
@@ -11,20 +11,18 @@
    ::pc/output [::m.users/email
                 ::m.users/name]}
   (timbre/infof "resolving user: %s" id)
-  (get sample/user-map id))
+  (q.users/read-record id))
 
 (defresolver users-resolver
   [_env _props]
   {::pc/output [{:all-users [::m.users/id]}]}
   {:all-users
-   (map (fn [id] [::m.users/id id]) (keys sample/user-map))})
-
-(defresolver user-map-resolver
-  [_env _props]
-  {::pc/output [::m.users/map]}
-  {::m.users/map sample/user-map})
+   (map (fn [id] [::m.users/id id]) (q.users/index-ids))})
 
 (def resolvers
   [user-resolver
-   users-resolver
-   user-map-resolver])
+   users-resolver])
+
+(comment
+  (q.users/index-ids)
+  (q.users/read-record 21))
