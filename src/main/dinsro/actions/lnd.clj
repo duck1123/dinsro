72160fd713fab3f2a758a3d03127bf4d579f63be
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-02-28 08:04:51 -0500

    add new methods for lnd support

--- src/main/dinsro/actions/lnd.clj
+++ src/main/dinsro/actions/lnd.clj
@@ -5,6 +5,8 @@
    [taoensso.timbre :as timbre])
   (:import
    java.io.File
+   org.lightningj.lnd.wrapper.message.ListChannelsRequest
+   org.lightningj.lnd.wrapper.message.ListInvoiceRequest
    org.lightningj.lnd.wrapper.SynchronousLndAPI))
 
 (defn get-api
@@ -18,6 +20,12 @@
   [m]
   (json/read-str (.toJsonAsString m false) :key-fn keyword))
 
+(defn list-channels
+  [api]
+  (let [request (ListChannelsRequest.)]
+    (.setActiveOnly request true)
+    (parse (.listChannels api request))))
+
 (defn list-peers
   [s-lnd-api]
   (parse (.listPeers s-lnd-api)))
@@ -26,14 +34,29 @@
   [s-lnd-api]
   (parse (.channelBalance s-lnd-api)))
 
+(defn index-invoices
+  [api]
+  (let [request (ListInvoiceRequest.)]
+    (parse (.listInvoices api request))))
+
 (comment
-  (let [wallet-base "/home/duck/.config/Zap/lnd/bitcoin/mainnet/wallet-2/"
-        config {:host "localhost"
-                :port 11009
-                :cert-path (str wallet-base "tls.cert")
-                :macaroon-path (str wallet-base "data/chain/bitcoin/mainnet/admin.macaroon")}
-        s-lnd-api (get-api config)]
-    (list-peers s-lnd-api)
-    (channel-balance s-lnd-api)
-    (spit "backups.txt"  (.exportAllChannelBackups s-lnd-api))
-    (puget/cprint (parse (.listPayments s-lnd-api false)))))
+  (def wallet-base "/usr/src/app/")
+
+  (def config {:host "192.168.0.28"
+               :port 10009
+               :cert-path (str wallet-base "tls.cert")
+               :macaroon-path (str wallet-base "admin.macaroon")})
+
+  (def s-lnd-api (get-api config))
+
+  (parse (.getInfo s-lnd-api))
+
+  (list-peers s-lnd-api)
+
+  (index-invoices s-lnd-api)
+
+  (first (:channels (list-channels s-lnd-api)))
+
+  (channel-balance s-lnd-api)
+  (spit "backups.txt"  (.exportAllChannelBackups s-lnd-api))
+  (puget/cprint (parse (.listPayments s-lnd-api false))))
