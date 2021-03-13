2dd0be165851855af929dbaf071bed2917bcc2f3
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-03-13 18:49:10 -0500

    Remove model file

--- src/main/dinsro/model.clj
+++
@@ -1,8 +0,0 @@
-(ns dinsro.model
-  (:require
-   [com.fulcrologic.rad.attributes :as attr]))
-
-(def all-attributes
-  (vec (concat)))
-
-(def all-attribute-validator (attr/make-attribute-validator all-attributes))
