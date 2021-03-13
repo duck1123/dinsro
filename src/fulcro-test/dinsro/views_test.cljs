211fc1978e8a6bddc8367204bf66870262dcc0fd
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-03-13 09:57:05 -0500

    Remove fulcro

--- src/fulcro-test/dinsro/views_test.cljs
+++
@@ -1,6 +0,0 @@
-(ns dinsro.views-test
-  (:require
-   [dinsro.views.admin-index-accounts-test]
-   [dinsro.views.index-accounts-test]
-   [dinsro.views.index-transactions-test]
-   [dinsro.views.registration-test]))
--- src/fulcro/dinsro/core.cljs
+++
@@ -1,7 +0,0 @@
-(ns dinsro.core
-  (:require
-   [taoensso.timbre :as timbre]))
-
-(defn init!
-  [debug?]
-  (timbre/infof "init - %s" debug?))
