929e45a7fa4c5f9e38fcac8997d0e79acab9bd2c
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-03-27 09:39:19 -0400

    Add initial support for async lnd api

--- src/main/dinsro/actions/lnd.clj
+++ src/main/dinsro/actions/lnd.clj
@@ -4,9 +4,11 @@
    [puget.printer :as puget]
    [taoensso.timbre :as timbre])
   (:import
+   io.grpc.stub.StreamObserver
    java.io.File
    org.lightningj.lnd.wrapper.message.ListChannelsRequest
    org.lightningj.lnd.wrapper.message.ListInvoiceRequest
+   org.lightningj.lnd.wrapper.AsynchronousLndAPI
    org.lightningj.lnd.wrapper.SynchronousLndAPI))
 
 (defn get-api
@@ -15,6 +17,14 @@
                       (:port config)
                       (File. (:cert-path config))
                       (File. (:macaroon-path config))))
+
+(defn get-async-api
+  [config]
+  (AsynchronousLndAPI.
+   (:host config)
+   (:port config)
+   (File. (:cert-path config))
+   (File. (:macaroon-path config))))
 
 (defn parse
   [m]
@@ -26,6 +36,19 @@
     (.setActiveOnly request true)
     (parse (.listChannels api request))))
 
+(defn balance-observer
+  []
+  (reify StreamObserver
+    (onNext [this note]
+      ;; there will be multiple .onNext here
+      (println (parse note)))
+    (onError [this err]
+      (println err))
+    (onCompleted [this]
+      (println "onCompleted server")
+      ;; there will be no .onNext here
+      #_(.onCompleted res))))
+
 (defn list-peers
   [s-lnd-api]
   (parse (.listPeers s-lnd-api)))
@@ -34,20 +57,30 @@
   [s-lnd-api]
   (parse (.channelBalance s-lnd-api)))
 
+(defn channel-balance-a
+  [a-lnd-api]
+  (.channelBalance a-lnd-api
+                   (balance-observer)))
+
 (defn index-invoices
   [api]
   (let [request (ListInvoiceRequest.)]
     (parse (.listInvoices api request))))
 
-(comment
-  (def wallet-base "/usr/src/app/")
+(def wallet-base "/usr/src/app/")
 
-  (def config {:host "192.168.0.28"
-               :port 10009
-               :cert-path (str wallet-base "tls.cert")
-               :macaroon-path (str wallet-base "admin.macaroon")})
+(def lnd-host "127.0.0.1")
+(def payment-request "")
+
+(def config {:host lnd-host
+             :port 10009
+             :cert-path (str wallet-base "tls.cert")
+             :macaroon-path (str wallet-base "admin.macaroon")})
+
+(comment
 
   (def s-lnd-api (get-api config))
+  (def a-lnd-api (get-async-api config))
 
   (parse (.getInfo s-lnd-api))
 
@@ -56,6 +89,29 @@
   (index-invoices s-lnd-api)
 
   (first (:channels (list-channels s-lnd-api)))
+
+  (channel-balance-a a-lnd-api)
+
+  (.getTransactions s-lnd-api
+                    ;; 0 100
+                    (int 0) (int 100) #_(balance-observer))
+
+  (.listPayments a-lnd-api
+                 true
+                 (long 0)
+                 (long 100)
+                 false
+                 (balance-observer))
+
+  (.listPeers a-lnd-api (balance-observer))
+
+  (list-peers s-lnd-api)
+
+  (puget/cprint (parse (.listPayments s-lnd-api false)))
+
+  (.getNumSatoshis (.decodePayReq s-lnd-api payment-request))
+
+  (channel-balance s-lnd-api)
 
   (channel-balance s-lnd-api)
   (spit "backups.txt"  (.exportAllChannelBackups s-lnd-api))
