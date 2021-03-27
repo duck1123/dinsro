9541f8f487b2dee7cebf1fba952e148878cfde92
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-03-27 11:12:42 -0400

    resolve categories from db

--- src/main/dinsro/resolvers/categories.clj
+++ src/main/dinsro/resolvers/categories.clj
@@ -3,25 +3,22 @@
    [com.wsscode.pathom.connect :as pc :refer [defresolver]]
    [dinsro.model.categories :as m.categories]
    [dinsro.model.users :as m.users]
-   [dinsro.sample :as sample]
+   [dinsro.queries.categories :as q.categories]
    [taoensso.timbre :as timbre]))
 
 (defresolver categories-resolver
   [_env _props]
   {::pc/output [{:all-categories [::m.categories/id]}]}
-  {:all-categories (map (fn [id] [::m.categories/id id])
-                        (keys sample/category-map))})
+  {:all-categories
+   (map (fn [id] [::m.categories/id id]) (q.categories/index-ids))})
 
 (defresolver category-resolver
   [_env {::m.categories/keys [id]}]
   {::pc/input #{::m.categories/id}
    ::pc/output [::m.categories/name
                 {::m.categories/user [::m.users/id]}]}
-  (get sample/category-map id))
+  (let [record (q.categories/read-record id)
+        id (:db/id record)]
+    (assoc record ::m.categories/id id)))
 
-(defresolver category-map-resolver
-  [_env _props]
-  {::pc/output [::m.categories/map]}
-  {::m.categories/map sample/category-map})
-
-(def resolvers [category-resolver categories-resolver category-map-resolver])
+(def resolvers [category-resolver categories-resolver])
