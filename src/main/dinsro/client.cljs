b0efd2114868258d5f8f2b76b34e4f5a6e03723d
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-04-12 18:28:47 -0400

    remove debug menu

--- src/main/dinsro/client.cljs
+++ src/main/dinsro/client.cljs
@@ -11,21 +11,17 @@
    [dinsro.routing :as routing]
    [dinsro.translations :refer [tr]]
    [dinsro.ui.bulma :as bulma]
-   [dinsro.ui.debug-menu :as u.debug-menu]
    [dinsro.ui.navbar :as u.navbar]
    [taoensso.timbre :as timbre]))
 
-(defsc Root [this {::keys [debug-links navbar router]}]
-  {:query         [{::debug-links (comp/get-query u.debug-menu/DebugLinkBar)}
-                   {::navbar (comp/get-query u.navbar/Navbar)}
+(defsc Root [this {::keys [navbar router]}]
+  {:query         [{::navbar (comp/get-query u.navbar/Navbar)}
                    {::router (comp/get-query router/RootRouter)}]
-   :initial-state {::debug-links {}
-                   ::navbar      {}
-                   ::router      {}}}
+   :initial-state {::navbar {}
+                   ::router {}}}
   (let [top-router-state (or (uism/get-active-state this ::router/RootRouter) :initial)]
     (dom/div
      (u.navbar/ui-navbar navbar)
-     (u.debug-menu/ui-debug-link-bar debug-links)
      (bulma/container
       (if (= :initial top-router-state)
         (dom/div :.loading "Loading...")
