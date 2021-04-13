866e42149bb666984d7c63acfba2195d884ff321
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-04-13 08:02:00 -0400

    authenticate

--- src/main/dinsro/actions/authentication.clj
+++ src/main/dinsro/actions/authentication.clj
@@ -4,7 +4,7 @@
    [buddy.sign.jwt :as jwt]
    [clj-time.core :as time]
    [clojure.spec.alpha :as s]
-   [com.fulcrologic.guardrails.core :refer [>defn =>]]
+   [com.fulcrologic.guardrails.core :refer [>defn ? =>]]
    [dinsro.actions.users :as a.users]
    [dinsro.config :refer [secret]]
    [dinsro.queries.users :as q.users]
@@ -13,37 +13,55 @@
    [ring.util.http-response :as http]
    [taoensso.timbre :as timbre]))
 
+(>defn authenticate
+  [email password]
+  [string? string? => (? (s/keys))]
+  (if-let [user (q.users/find-by-email email)]
+    (if-let [password-hash (::m.users/password-hash user)]
+      (if (hashers/check password password-hash)
+        (let [id     (::m.users/id user)
+              claims {:user id
+                      :exp  (time/plus (time/now) (time/minutes 3600))}]
+          {::s.a.authentication/identity id
+           :token                        (jwt/sign claims secret)})
+        ;; Password does not match
+        (do
+          (timbre/info "password not matched")
+          nil))
+      (do
+        ;; No password, invalid user
+        (timbre/info "no password")
+        nil))
+
+    (do
+      ;; User not found
+      (timbre/info "user not found")
+      nil)))
+
 (>defn authenticate-handler
   [request]
   [::s.a.authentication/authenticate-request => ::s.a.authentication/authenticate-response]
   (let [{{:keys [email password]} :params} request]
-    (if (and (seq email) (seq password))
-      (if-let [user (q.users/find-by-email email)]
-        (if-let [password-hash (::m.users/password-hash user)]
-          (if (hashers/check password password-hash)
-            (let [id     (::m.users/id user)
-                  claims {:user id
-                          :exp  (time/plus (time/now) (time/minutes 3600))}]
-              (-> {::s.a.authentication/identity id
-                   :token                        (jwt/sign claims secret)}
-                  (http/ok)
-                  (assoc-in [:session :identity] id)))
-            ;; Password does not match
-            (http/unauthorized {:status :unauthorized}))
-          ;; No password, invalid user
+    (try
+      (if (and (seq email) (seq password))
+        (if-let [claim (authenticate email password)]
+          (let [id (::s.a.authentication/identity claim)]
+            (-> claim
+                (http/ok)
+                (assoc-in [:session :identity] id)))
           (http/unauthorized {:status :unauthorized}))
-        ;; User not found
-        (http/unauthorized {:status :unauthorized}))
-      (http/bad-request {:status :invalid}))))
+        (http/bad-request {:status :invalid}))
+      (catch Exception ex
+        (timbre/error ex "Failed to authenticate")))))
 
 (>defn register
   [params]
   [::m.users/params => ::m.users/item]
-  (let [params           (if-let [password (:password params)]
-                           (assoc params ::m.users/password password)
-                           params)
-        params           (dissoc params :password)
-        params           (a.users/prepare-record params)]
+  (let [params (if-let [password (:password params)]
+                 (assoc params ::m.users/password password)
+                 params)
+        params (dissoc params :password)
+        params (a.users/prepare-record params)]
     (if (s/valid? ::m.users/params params)
       (try
         (let [id (q.users/create-record params)]
--- src/main/dinsro/mutations/session.clj
+++ src/main/dinsro/mutations/session.clj
@@ -4,6 +4,7 @@
    [com.wsscode.pathom.connect :as pc :refer [defmutation]]
    [dinsro.actions.authentication :as a.authentication]
    [dinsro.model.users :as m.users]
+   [dinsro.queries.users :as q.users]
    [taoensso.timbre :as timbre]))
 
 (defmutation register
@@ -18,12 +19,15 @@
   [{{:keys [session]} :request} {:user/keys [email password]}]
   {::pc/params #{:user/email :user/password}
    ::pc/output [:user/id :user/valid?]}
-  (if (= password "hunter2")
-    (augment-response
-     {:user/id     email
-      :user/valid? true}
-     (fn [ring-response]
-       (assoc ring-response :session (assoc session :identity email))))
+  (if-let [_user (q.users/find-by-email email)]
+    (if (= password "hunter2")
+      (augment-response
+       {:user/id     email
+        :user/valid? true}
+       (fn [ring-response]
+         (assoc ring-response :session (assoc session :identity email))))
+      {:user/id     nil
+       :user/valid? false})
     {:user/id     nil
      :user/valid? false}))
 
