55be57464874d7784e98de11ba21f144d8fb1409
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-04-14 08:00:02 -0400

    users test

--- src/test/dinsro/queries/users_test.clj
+++ src/test/dinsro/queries/users_test.clj
@@ -6,6 +6,7 @@
    [dinsro.queries.users :as q.users]
    [dinsro.specs :as ds]
    [dinsro.test-helpers :as th]
+   [fulcro-spec.core :refer [assertions behavior specification]]
    [taoensso.timbre :as timbre]))
 
 (def schemata
@@ -13,26 +14,28 @@
 
 (use-fixtures :each (fn [f] (th/start-db f schemata)))
 
-(deftest create-record-valid
-  (let [{::m.users/keys [email]
-         :as            params} (ds/gen-key ::m.users/params)
-        id                      (q.users/create-record params)
-        user                    (q.users/read-record id)]
-    (is (= email (::m.users/email user)))))
+(specification "create-record"
+  (behavior "when the params are valid"
+    (let [params                   (ds/gen-key ::m.users/params)
+          {::m.users/keys [email]} params
+          id                       (q.users/create-record params)
+          user                     (q.users/read-record id)]
+      (assertions
+       email => (::m.users/email user))))
+  (behavior "when the params are invalid"
+    (let [params (ds/gen-key ::m.users/params)
+          _id    (q.users/create-record params)]
+      (assertions
+       (q.users/create-record params) =throws=> RuntimeException))))
 
-(deftest create-record-invalid
-  (let [params (ds/gen-key ::m.users/params)
-        id     (q.users/create-record params)]
-    (q.users/read-record id)
-    ;; TODO: Throw a better error
-    (is (thrown? RuntimeException (q.users/create-record params)))))
-
-(deftest read-record
-  (let [{::m.users/keys [email]
-         :as            params} (ds/gen-key ::m.users/params)
-        id                      (q.users/create-record params)
-        response                (q.users/read-record id)]
-    (is (= email (::m.users/email response)))))
+(specification "read-record"
+  (behavior "when the record exists"
+    (let [params                   (ds/gen-key ::m.users/params)
+          {::m.users/keys [email]} params
+          id                       (q.users/create-record params)
+          response                 (q.users/read-record id)]
+      (assertions
+       email => (::m.users/email response)))))
 
 (deftest read-record-missing
   (let [id       (ds/gen-key :db/id)
