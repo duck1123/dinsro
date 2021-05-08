2b252275c645ec8bbc9e59f2c986136f54319aaf
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-05-08 11:22:34 -0400

    index currencies

--- src/test/dinsro/ui/index_currencies_test.cljs
+++ src/test/dinsro/ui/index_currencies_test.cljs
@@ -1,6 +1,6 @@
 (ns dinsro.ui.index-currencies-test
   (:require
-   [dinsro.sample :as sample]
+   [dinsro.model.currencies :as m.currencies]
    [dinsro.translations :refer [tr]]
    [dinsro.ui.index-currencies :as u.index-currencies]
    [nubank.workspaces.card-types.fulcro3 :as ct.fulcro3]
@@ -11,6 +11,11 @@
   {::wsm/card-height 7
    ::wsm/card-width  2}
   (ct.fulcro3/fulcro-card
-   {::ct.fulcro3/root       u.index-currencies/IndexCurrencies
+   {::ct.fulcro3/root u.index-currencies/IndexCurrencies
     ::ct.fulcro3/initial-state
-    (fn [] {::u.index-currencies/currencies (map sample/currency-map ["sats" "yen" "eur"])})}))
+    (fn []
+      {::u.index-currencies/currencies
+       [{::m.currencies/id "sats"
+         ::m.currencies/link
+         [{::m.currencies/id   "sats"
+           ::m.currencies/name "Sats"}]}]})}))
