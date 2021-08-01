86b3978503c73e9945dff5e9a5482ec238fd5c6e
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-08-01 11:53:31 -0400

    show currency

--- src/main/dinsro/views/show_currency.cljs
+++ src/main/dinsro/views/show_currency.cljs
@@ -11,13 +11,13 @@
 
 (defsc ShowCurrencyPage
   [_this {::keys [currency]}]
-  {:ident         (fn [] [:page/id ::page])
+  {:ident (fn [] [:page/id ::page])
    :initial-state {::currency {}}
    :query         [{::currency (comp/get-query u.show-currency/ShowCurrencyFull)}]
-   :route-segment ["currencies" :id]
+   :route-segment ["currencies" ::m.currencies/id]
    :will-enter
-   (fn [app {:keys [id]}]
-     (when (seq id)
+   (fn [app {::m.currencies/keys [id]}]
+     (when id
        (df/load app [::m.currencies/id (new-uuid id)] u.show-currency/ShowCurrencyFull
                 {:target [:page/id ::page ::currency]}))
      (dr/route-immediate (comp/get-ident ShowCurrencyPage {})))}
