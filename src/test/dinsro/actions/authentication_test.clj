aeea57021760d1eddafc7bc9510efa18120f11a3
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-10-31 12:11:25 -0400

    Auth action test

---
+++ src/test/dinsro/actions/authentication_test.clj
@@ -0,0 +1,20 @@
+(ns dinsro.actions.authentication-test
+  (:require
+   [clojure.test :refer [deftest use-fixtures]]
+   [dinsro.actions.authentication :as a.authentication]
+   [dinsro.model.users :as m.users]
+   [dinsro.specs :as ds]
+   [dinsro.test-helpers :as th]
+   [fulcro-spec.core :refer [assertions]]
+   [taoensso.timbre :as log]))
+
+(def schemata [])
+
+(use-fixtures :each (fn [f] (th/start-db f schemata)))
+
+(deftest do-register-success
+  (let [password (ds/gen-key ::m.users/password)
+        username (ds/gen-key ::m.users/name)
+        response (a.authentication/do-register username password)]
+    (assertions
+     (::m.users/name response) => username)))
