f74cd19ff19b30956c73d51ffba81d0d6ed343af
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-11-06 09:03:30 -0400

    Remove render method

--- src/main/dinsro/layout.clj
+++ src/main/dinsro/layout.clj
@@ -1,26 +1,9 @@
 (ns dinsro.layout
   (:require
    [clojure.java.io :as io]
-   [selmer.parser :as parser]
-   [ring.util.response]
-   [ring.util.http-response :refer [content-type ok]]
-   [ring.util.anti-forgery :refer [anti-forgery-field]]
-   [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
+   [selmer.parser :as parser]))
 
 (parser/set-resource-path!  (io/resource "html"))
-(parser/add-tag! :csrf-field (fn [_ _] (anti-forgery-field)))
-
-(defn render
-  "renders the HTML template located relative to resources/templates"
-  [template & [params]]
-  (content-type
-   (ok
-    (parser/render-file
-     template
-     (assoc params
-            :page template
-            :csrf-token *anti-forgery-token*)))
-   "text/html; charset=utf-8"))
 
 (defn error-page
   "error-details should be a map containing the following keys:
