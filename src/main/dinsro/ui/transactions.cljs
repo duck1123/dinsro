f4ec5c4dd7bb3748062495b55477ebc823f2567a
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-11-06 08:44:20 -0400

    Transaction form

--- src/main/dinsro/ui/transactions.cljs
+++ src/main/dinsro/ui/transactions.cljs
@@ -13,17 +13,20 @@
    [dinsro.ui.links :as u.links]
    [taoensso.timbre :as log]))
 
-(form/defsc-form TransactionForm [_this _props]
-  {fo/id           m.transactions/id
-   fo/attributes   []
-   fo/route-prefix "transaction"
-   fo/title        "Transaction"})
-
 (defattr transaction-account-link ::m.transactions/account :ref
   {ao/cardinality      :one
    ao/identities       #{::m.transactions/id}
    ao/target           ::m.accounts/id
    ::report/column-EQL {::m.transactions/account (comp/get-query u.links/AccountLink)}})
+
+(form/defsc-form TransactionForm [_this _props]
+  {fo/id           m.transactions/id
+   fo/attributes   [m.transactions/description
+                    ;; m.transactions/account
+                    transaction-account-link]
+   fo/field-styles {::m.transactions/account :link}
+   fo/route-prefix "transaction"
+   fo/title        "Transaction"})
 
 (report/defsc-report TransactionsReport
   [_this _props]
@@ -35,6 +38,7 @@
                                            :type   :button
                                            :action (fn [this] (form/create! this TransactionForm))}}
    ro/control-layout   {:action-buttons [::new-transaction]}
+   ro/form-links       {::m.transactions/description TransactionForm}
    ro/field-formatters
    {::m.transactions/account (fn [_this props] (u.links/ui-account-link props))}
    ro/route            "transactions"
