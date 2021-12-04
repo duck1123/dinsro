e0560b5663d922404f27323fe2821b989c9f75a0
Duck Nebuchadnezzar <duck@kronkltd.net>
2021-12-04 07:07:44 -0500

    Update command dispatching to allow multiple commands

--- src/dispatch/dinsro/components/dispatch.clj
+++ src/dispatch/dinsro/components/dispatch.clj
@@ -1,13 +1,15 @@
 (ns dinsro.components.dispatch
   (:require
    [nrepl.core :as nrepl]
-   [nrepl.cmdline :as cmdline]
    [nrepl.misc :refer [noisy-future]]
    [nrepl.transport :as transport]
    [taoensso.timbre :as log]))
 
+(def repl-host "127.0.0.1")
+(def repl-port 7000)
+
 (defn repl-fn
-  [host port cmd]
+  [host port cmds]
   (let [transport (nrepl/connect :host host :port port :transport-fn #'transport/bencode)
         client    (nrepl/client transport Long/MAX_VALUE)]
     (noisy-future
@@ -15,13 +17,15 @@
           (take-while #(nil? (:id %)))
           (run! #(when-let [msg (:out %)] (print msg)))))
     (let [session (nrepl/client-session client)]
-      (doseq [res (nrepl/message session {:op "eval" :code cmd})]
-        (comment
-          (when-let [out (:out res)]
-            (println out)))))))
+      (doseq [cmd cmds]
+        (doseq [{:keys [out value]
+                 :as   res} (nrepl/message session {:op "eval" :code cmd})]
+          (cond
+            out   (println out)
+            value (println value)
+            :else (println res)))))))
 
 (defn -main
-  []
-  (repl-fn "127.0.0.1" 7000 "(restart)")
-  (println "done")
+  [& args]
+  (repl-fn repl-host repl-port args)
   (System/exit 0))
