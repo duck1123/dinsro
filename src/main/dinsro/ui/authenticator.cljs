c96921772f0466ee78f88a5f3c39ec86b9dd0922
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-01-30 09:36:32 -0500

    update home page

--- src/main/dinsro/ui/authenticator.cljs
+++ src/main/dinsro/ui/authenticator.cljs
@@ -1,9 +1,20 @@
 (ns dinsro.ui.authenticator
   (:require
-   [com.fulcrologic.fulcro.components :as comp]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.rad.authorization :as auth]
+   [dinsro.mutations.session :as mu.session]
    [dinsro.ui.login :refer [LoginPage]]))
 
 (auth/defauthenticator Authenticator {:local LoginPage})
 
 (def ui-authenticator (comp/factory Authenticator))
+
+(defsc LocalData
+  [_ _]
+  {:query         [{[:com.fulcrologic.rad.authorization/authorization :local] (comp/get-query mu.session/Session)}]
+   :initial-state {}})
+
+(defsc UserAuthenticator
+  [_ _]
+  {:query         [{:local (comp/get-query LocalData)}]
+   :initial-state {:local {}}})
--- src/main/dinsro/ui/home.cljs
+++ src/main/dinsro/ui/home.cljs
@@ -2,13 +2,17 @@
   (:require
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.dom :as dom]
+   [com.fulcrologic.fulcro-css.css :as css]
    [com.fulcrologic.rad.control :as control]
    [com.fulcrologic.rad.control-options :as copt]
    [com.fulcrologic.rad.container :as container :refer [defsc-container]]
    [com.fulcrologic.rad.container-options :as co]
+   [dinsro.model.users :as m.users]
    [dinsro.ui.accounts :as u.accounts]
+   [dinsro.ui.authenticator :as u.authenticator]
    [dinsro.ui.categories :as u.categories]
-   [dinsro.ui.ln-nodes :as u.ln-nodes]))
+   [dinsro.ui.ln-nodes :as u.ln-nodes]
+   [lambdaisland.glogc :as log]))
 
 (defsc-container HomePage2
   [_this _props]
@@ -30,9 +34,25 @@
    copt/control-layout {:action-buttons [::refresh]}})
 
 (defsc HomePage
-  [_this _props]
+  [_this {:root/keys [authenticator]}]
   {:route-segment [""]
-   :query []
-   :initial-state {}
-   :ident (fn [] [:component/id ::HomePage])}
-  (dom/div {} "Home Page"))
+   :query         [:component/id
+                   {:root/authenticator (comp/get-query u.authenticator/UserAuthenticator)}]
+   :initial-state {:component/id       ::HomePage
+                   :root/authenticator {}}
+   :css           [[:.container {:background-color "white"}]
+                   [:.title {:color       "blue"
+                             :font-weight "bold"}]]
+   :ident         (fn [] [:component/id ::HomePage])}
+  (log/info :home-page/rendered {:authenticator authenticator})
+  (let [{:keys [container title]} (css/get-classnames HomePage)
+        username                  (get-in authenticator [:local [:com.fulcrologic.rad.authorization/authorization :local] :session/current-user ::m.users/name])]
+    (comp/fragment
+     (dom/div {:classes [:.ui.inverted.vertical.masthead.center.aligned.segment container]}
+       (dom/div :.ui.container
+         (dom/div :.ui.text.container
+           (dom/h1 {:classes [:.ui.inverted.header title]}
+                   (if username
+                     (str "Welcome, " username)
+                     "Home Page"))
+           (dom/h2 {} "TODO: put stuff here")))))))
