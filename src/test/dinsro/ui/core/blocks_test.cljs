7e25b488798107fe3a7bcb38d362882dede95c2a
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-03-27 15:52:33 -0400

    blocks workspaces

---
+++ src/test/dinsro/ui/core/blocks_test.cljs
@@ -0,0 +1,33 @@
+(ns dinsro.ui.core.blocks-test
+  (:require
+   [dinsro.model.core.blocks :as m.c.blocks]
+   [dinsro.ui.core.blocks :as u.c.blocks]
+   [nubank.workspaces.card-types.fulcro3 :as ct.fulcro3]
+   [nubank.workspaces.core :as ws]))
+
+(ws/defcard RefRow
+  (ct.fulcro3/fulcro-card
+   {::ct.fulcro3/root u.c.blocks/RefRow
+    ::ct.fulcro3/initial-state
+    (fn [] {::m.c.blocks/id       :foo
+            ::m.c.blocks/fetched? true
+            ::m.c.blocks/height   6
+            ::m.c.blocks/hash     "yes"})}))
+
+(ws/defcard CoreBlockSubForm
+  (ct.fulcro3/fulcro-card
+   {::ct.fulcro3/root u.c.blocks/CoreBlockSubForm
+    ::ct.fulcro3/initial-state
+    (fn [] {::m.c.blocks/id :foo
+            ::m.c.blocks/fetched? true
+            ::m.c.blocks/height 6
+            ::m.c.blocks/hash "yes"})}))
+
+(ws/defcard CoreBlockReport
+  (ct.fulcro3/fulcro-card
+   {::ct.fulcro3/root u.c.blocks/CoreBlockReport
+    ::ct.fulcro3/initial-state
+    (fn [] {::m.c.blocks/id       :foo
+            ::m.c.blocks/fetched? true
+            ::m.c.blocks/height   6
+            ::m.c.blocks/hash     "yes"})}))
