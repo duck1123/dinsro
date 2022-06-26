f08cd0365fd51ad466138fb0b0768c3588e90ca6
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-06-26 10:25:20 -0400

    remove prone

--- src/dev/dinsro/dev_middleware.clj
+++ src/dev/dinsro/dev_middleware.clj
@@ -1,11 +1,9 @@
 (ns dinsro.dev-middleware
   (:require
-   [prone.middleware :refer [wrap-exceptions]]
    [ring.middleware.reload :refer [wrap-reload]]
    [selmer.middleware :refer [wrap-error-page]]))
 
 (defn wrap-dev [handler]
   (-> handler
       wrap-reload
-      wrap-error-page
-      wrap-exceptions))
+      wrap-error-page))
