3d1f39bf0e0bf26f3b4d49d69fbf4b2dce3f54f9
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-02-17 19:47:01 -0500

    event networks

---
+++ src/notebooks/dinsro/actions/nostr/events_notebook.clj
@@ -0,0 +1,15 @@
+^{:nextjournal.clerk/visibility #{:hide-ns}}
+(ns dinsro.actions.nostr.events-notebook
+  (:require
+   [dinsro.model.nostr.events :as m.n.events]
+   [dinsro.notebook-utils :as nu]
+   [dinsro.specs :as ds]
+   [dinsro.viewers :as dv]
+   [nextjournal.clerk :as clerk]))
+
+;; # Nostr Events
+
+^{::clerk/viewer dv/file-link-viewer ::clerk/visibility {:code :hide}}
+(nu/display-file-links)
+
+(ds/gen-key ::m.n.events/item)
