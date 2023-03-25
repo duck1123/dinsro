b1ffbf3e61731a572ef4adc42059751ad8bfbdb3
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-03-25 15:03:16 -0400

    Remove dev middleware

--- src/dev/dinsro/dev_middleware.clj
+++
@@ -1,9 +0,0 @@
-(ns dinsro.dev-middleware
-  (:require
-   [ring.middleware.reload :refer [wrap-reload]]
-   [selmer.middleware :refer [wrap-error-page]]))
-
-(defn wrap-dev [handler]
-  (-> handler
-      wrap-reload
-      wrap-error-page))
--- src/dev/dinsro/env.clj
+++ src/dev/dinsro/env.clj
@@ -1,16 +1,13 @@
 (ns dinsro.env
   (:require
-   [dinsro.dev-middleware :refer [wrap-dev]]
-   [lambdaisland.glogc :as log]
-   [selmer.parser :as parser]))
+   [lambdaisland.glogc :as log]))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (def defaults
   {:init
    (fn []
-     (parser/cache-off!)
      (log/info :defaults/starting {}))
    :stop
    (fn []
      (log/info :defaults/stopping {}))
-   :middleware wrap-dev})
+   :middleware identity})
