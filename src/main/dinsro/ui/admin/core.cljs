d632fa36ead604ff760529ff35e91090d5cec52b
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-05-12 00:37:51 -0400

    extract core dashboard

--- src/main/dinsro/ui/admin/core.cljs
+++ src/main/dinsro/ui/admin/core.cljs
@@ -1,25 +1,21 @@
 (ns dinsro.ui.admin.core
   (:require
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom]
    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr :refer [defrouter]]
    [dinsro.menus :as me]
+   [dinsro.ui.admin.core.blocks :as u.a.c.blocks]
+   [dinsro.ui.admin.core.dashboard :as u.a.c.dashboard]
+   [dinsro.ui.admin.core.peers :as u.a.c.peers]
    [dinsro.ui.links :as u.links]))
 
 (def router-key :dinsro.ui.admin/Router)
 
-(defsc Dashboard
-  [_this _props]
-  {:ident         (fn [] [:component/id ::Dashboard])
-   :initial-state {}
-   :query         [[::dr/id router-key]]
-   :route-segment ["dashboard"]}
-  (dom/div {}
-    (dom/h1 "Dashboard")))
-
 (defrouter Router
   [_this _props]
-  {:router-targets [Dashboard]})
+  {:router-targets
+   [u.a.c.dashboard/Dashboard
+    u.a.c.blocks/Report
+    u.a.c.peers/Report]})
 
 (defsc Page
   [_this {:ui/keys [router]}]
@@ -29,5 +25,4 @@
    :route-segment ["core"]}
   (comp/fragment
    (u.links/ui-nav-menu {:menu-items me/admin-core-menu-items :id nil})
-
    ((comp/factory Router) router)))
---
+++ src/main/dinsro/ui/admin/core/dashboard.cljs
@@ -0,0 +1,16 @@
+(ns dinsro.ui.admin.core.dashboard
+  (:require
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+   [com.fulcrologic.fulcro.dom :as dom]
+   [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]))
+
+(def router-key :dinsro.ui.admin/Router)
+
+(defsc Dashboard
+  [_this _props]
+  {:ident         (fn [] [:component/id ::Dashboard])
+   :initial-state {}
+   :query         [[::dr/id router-key]]
+   :route-segment ["dashboard"]}
+  (dom/div :.ui.segment
+    (dom/h1 "Dashboard")))
