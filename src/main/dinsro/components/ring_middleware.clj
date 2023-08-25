4a0ae2eebe7a3ba7010ace64415ff7765419c301
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-08-25 18:32:46 -0400

    add start url to manifest

--- src/main/dinsro/components/ring_middleware.clj
+++ src/main/dinsro/components/ring_middleware.clj
@@ -135,6 +135,8 @@
     :icons
     [{:src "/images/android-chrome-192x192.png" :sizes "192x192" :type "image/png"}
      {:src "/images/android-chrome-512x512.png" :sizes "512x512" :type "image/png"}]
+    :id               "/?source=pwa"
+    :start_url        "/?source=pwa"
     :theme_color      "#ffffff"
     :background_color "#ffffff"
     :display          "standalone"}))
