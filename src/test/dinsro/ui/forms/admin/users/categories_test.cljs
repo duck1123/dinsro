6a020aa47aa3995a7162d11f4290e63693ce975b
Duck Nebuchadnezzar <duck@kronkltd.net>
2023-12-13 12:37:10 -0500

    fix categories test

--- src/test/dinsro/ui/forms/admin/users/categories_test.cljs
+++ src/test/dinsro/ui/forms/admin/users/categories_test.cljs
@@ -2,13 +2,15 @@
   (:require
    [dinsro.client :as client]
    [dinsro.mocks.ui.forms.admin.users.categories :refer [make-body-item]]
-   [dinsro.ui.admin.users.categories :as u.a.u.categories]
+   [dinsro.ui.forms.admin.users.categories :as u.f.a.u.categories]
    [nubank.workspaces.card-types.fulcro3 :as ct.fulcro3]
    [nubank.workspaces.core :as ws]))
+
+;; [[../../../../../../main/dinsro/ui/forms/admin/users/categories.cljc]]
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (ws/defcard AdminUserCategoriesNewForm
   (ct.fulcro3/fulcro-card
-   {::ct.fulcro3/root          u.a.u.categories/NewForm
+   {::ct.fulcro3/root          u.f.a.u.categories/NewForm
     ::ct.fulcro3/app           {:client-will-mount client/setup-RAD}
     ::ct.fulcro3/initial-state (fn [] (make-body-item))}))
