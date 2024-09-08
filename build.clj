ef060c6610af81d0e0bd451e47b6829439a20287
Duck Nebuchadnezzar <duck@kronkltd.net>
2024-09-08 18:29:21 -0400

    package clerk asset-map when uberjarring

--- build.clj
+++ build.clj
@@ -1,7 +1,10 @@
 (ns build
   "dinsro build script"
-  (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.tools.build.api :as b]
+   [nextjournal.clerk.config :as config]
+   [org.corfield.build :as bb]))
 
 (def lib 'duck1123/dinsro)
 (def version (format "4.0.%s" (b/git-count-revs nil)))
@@ -43,10 +46,24 @@
 (defn eastwood "Run Eastwood." [opts]
   (-> opts (bb/run-task [:eastwood])))
 
+(defn package-asset-map [_]
+  (let [lookup-url "https://storage.googleapis.com/nextjournal-cas-eu/assets/4AHiU1U3uJCbnLcyv1qxbfFjKqfjz8RqQBZ5bxyLhH8dnxqWSc8tsQHfzCxGggVwwRv8EvZYoVoSuMTm3UdddqoN-viewer.js"
+        #_#_asset-map (slurp lookup-url)]
+    (io/make-parents "target/classes/clerk-asset-map.edn")
+    (spit "target/classes/clerk-asset-map.edn" {"/js/viewer.js" lookup-url})))
+
+(defn package-clerk-asset-map [{:as opts :keys [target-dir]}]
+  (when-not target-dir
+    (throw (ex-info "target dir must be set" {:opts opts})))
+  (let [asset-map @config/!asset-map]
+    (spit (str target-dir java.io.File/separator "clerk-asset-map.edn") asset-map)))
+
+
 (defn uber [opts]
   (b/copy-dir
    {:src-dirs   ["src/notebooks"]
     :target-dir (str class-dir "/src/notebooks")})
+  (package-clerk-asset-map {:target-dir class-dir})
   (-> opts
       (merge {:lib           lib
               :version       version
